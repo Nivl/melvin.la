3b85480d632346b69ca6f253daf5cc8a719e589b
Nivl <nivl@free.fr>
2010-09-09 17:44:25 +0200

    Changes the way the current page is displayed in the URL.

--- nivlsblog/categories/views.py
+++ nivlsblog/categories/views.py
@@ -3,17 +3,13 @@ from nivlsblog.entries.models    import Entry
 from django.core.paginator       import Paginator, InvalidPage, EmptyPage
 from django.shortcuts            import render_to_response, get_object_or_404, get_list_or_404
 
-def show_entries(request, slug):
+def show_entries(request, slug, page):
     cat = get_object_or_404(Category, slug=slug)
     cat_list = Category.objects.filter(left__gte=cat.left,
                                        right__lte=cat.right)
     entry_list = Entry.objects.select_related(depth=2).filter(category__in=cat_list).order_by('-date')
 
     paginator = Paginator(entry_list, 5)
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
     try:
         entries = paginator.page(page)
     except (EmptyPage, InvalidPage):
