f395543ea2be520c418c3f1f8abae9d5a3d2aa86
Nivl <nivl@free.fr>
2010-09-11 05:39:28 +0200

    Adds entries detail (and changes entries link).

--- nivlsblog/entries/urls.py
+++ nivlsblog/entries/urls.py
@@ -1,6 +1,23 @@
 from django.conf.urls.defaults import *
+from nivlsblog.entries.models  import Entry
+from django.views.generic import date_based
 
-urlpatterns = patterns('nivlsblog.entries.views',
+
+entry_info = {'queryset'                    : Entry.objects.all(),
+              'date_field'                  : 'date',
+              'template_name'               : 'entries/detail.html',
+              'template_object_name'        : 'entry',
+              'month_format'                : '%m'}
+
+urlpatterns = patterns('',
     (r'^comments/', include('django.contrib.comments.urls')),
-    (r'^(?P<entry_id>\d+)/$', 'detail'),
-)
+    (url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)$',
+         date_based.object_detail, entry_info)),
+    )
+
+
+#tt = time.strptime('%s-%s-%s' % (2010, 09, 09),
+#                   '%s-%s-%s' % ('%Y', '', ''))
+#date = datetime.date(*tt[:3])
+
+#print date
