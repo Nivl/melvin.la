2e257878617bc7d2bba3608c86c953d30d89ba33
Nivl <nivl@free.fr>
2010-09-11 03:28:48 +0200

    Adds a '/' at the end of the URLs when they end with a tag slug or a category slug.

--- nivlsblog/categories/urls.py
+++ nivlsblog/categories/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('nivlsblog.categories.views',
-    (r'^(?P<slug>[a-zA-Z0-9_.-]+)$', 'show_entries', {'page': 1}),
+    (r'^(?P<slug>[a-zA-Z0-9_.-]+)/$', 'show_entries', {'page': 1}),
     (r'^(?P<slug>[a-zA-Z0-9_.-]+)/page-(?P<page>[0-9]+)$', 'show_entries')
 )
 
