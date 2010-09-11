8cd2a2abdda8bcd338a4a9dd496d7ef83fb8f649
Nivl <nivl@free.fr>
2010-09-11 03:42:52 +0200

    Adds the entry list as main page.

--- nivlsblog/entries/views.py
+++ nivlsblog/entries/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.context_processors import csrf
 from nivlsblog.entries.models import Entry
+from nivlsblog.libs.simple_paginator import simple_paginator
 from datetime import datetime
 
 
-def index(request):
-    latest_entry_list = Entry.objects.all().order_by('-date')[:5]
-    return render_to_response('entries/index.html',
-                              {'latest_entry_list': latest_entry_list})
+def listing(request, page):
+    entry_list = Entry.objects.select_related(depth=2).order_by('-date')
+    entries = simple_paginator(entry_list, 5, page)
+    return render_to_response('entries/list.html',
+                              {'entries': entries})
 
 
 def detail(request, entry_id):
--- nivlsblog/urls.py
+++ nivlsblog/urls.py
@@ -6,7 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
-    (r'^$', 'nivlsblog.main.views.index'),
+    (r'^$', 'nivlsblog.entries.views.listing', {'page': 1}),
+    (r'^page-(?P<page>[0-9]+)$', 'nivlsblog.entries.views.listing'),
     (r'^feeds/', include('nivlsblog.feeds.urls')),
     (r'^contact/$', 'nivlsblog.main.views.contact'),
     (r'^entries/', include('nivlsblog.entries.urls')),
