9760865870bf1108f8e365bc2ac5833863116010
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-08-17 21:23:44 +0200

    Fixes loading.gif path
    Changes the speed of the slide

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,32 +1,25 @@
 $(function(){
     $('#slides').slides({
 	preload: true,
-	preloadImage: '../img/slideshow/loading.gif',
+	preloadImage: STATIC_URL + 'img/slideshow/loading.gif',
 	play: 5000,
 	pause: 2500,
+	slideSpeed: 600,
 	hoverPause: true,
 	animationStart: function(current){
 	    $('.caption').animate({
-		bottom:-35
-	    },100);
-	    if (window.console && console.log) {
-		// example return of current slide number
-		console.log('animationStart on slide: ', current);
-	    };
+		bottom: -35
+ 	    }, 100);
 	},
 	animationComplete: function(current){
 	    $('.caption').animate({
-		bottom:0
-	    },200);
-	    if (window.console && console.log) {
-		// example return of current slide number
-		console.log('animationComplete on slide: ', current);
-	    };
+		bottom: 0
+	    }, 200);
 	},
 	slidesLoaded: function() {
 	    $('.caption').animate({
-		bottom:0
-	    },200);
+		bottom: 0
+	    }, 200);
 	}
     });
 });
