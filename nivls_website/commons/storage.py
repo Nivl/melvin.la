b86023769145735e426dd0e3e936149aafe12603
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-08-25 22:34:11 +0200

    Adds a new class to handle the file storage.

---
+++ nivls_website/commons/storage.py
@@ -0,0 +1,19 @@
+from django.core.files.storage import FileSystemStorage
+import os
+import uuid
+
+class UniqueFileSystemStorage(FileSystemStorage):
+    def get_unique_filename(self, filename):
+        root, ext = os.path.splitext(filename)
+        filename = "%s.%s" % (uuid.uuid4(), ext)
+        return os.path.join("uploaded_files", filename)
+
+    def get_valid_name(self, name):
+        return self.get_unique_filename(name)
+
+    def get_available_name(self, name):
+        dirname, filename = os.path.split(name)
+        file_root, file_ext = os.path.splitext(filename)
+        while self.exists(name):
+            name = os.path.join(dirname, self.get_unique_filename(filename))
+        return name
