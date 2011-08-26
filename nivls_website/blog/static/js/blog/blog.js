9d7509549898ecdc2b7251dd02b52d17c4a0a698
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-08-26 15:18:49 +0200

    Moves static files to the blog subdirectory

---
+++ nivls_website/blog/static/js/blog/blog.js
@@ -0,0 +1,25 @@
+$(function(){
+    $('#slides').slides({
+	preload: true,
+	preloadImage: STATIC_URL + 'img/blog/slideshow/loading.gif',
+	play: 5000,
+	pause: 2500,
+	slideSpeed: 600,
+	hoverPause: true,
+	animationStart: function(current){
+	    $('.caption').animate({
+		bottom: -35
+ 	    }, 100);
+	},
+	animationComplete: function(current){
+	    $('.caption').animate({
+		bottom: 0
+	    }, 200);
+	},
+	slidesLoaded: function() {
+	    $('.caption').animate({
+		bottom: 0
+	    }, 200);
+	}
+    });
+});
