a97737976d3ce92edcc400375929e4c07b17d4e8
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-09-03 16:00:00 +0200

    Replaces django-categories by blog.category

--- nivls_website/blog/admin.py
+++ nivls_website/blog/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from blog.models import Post, SeeAlso
+from blog.models import Post, SeeAlso, Category
 
 class AdminPost(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
+class AdminCategory(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_ordering(self, request):
+        return ["left"]
+
 admin.site.register(Post, AdminPost)
 admin.site.register(SeeAlso)
+admin.site.register(Category, AdminCategory)
--- nivls_website/blog/models.py
+++ nivls_website/blog/models.py
@@ -1,19 +1,41 @@
 import os
 import datetime
 from django.db import models
-from categories.models import Category
 from tags.models import Tag
 from django.contrib.auth.models import User
 from images.models import Image
+
+class Category(models.Model):
+    name          = models.CharField(max_length=50)
+    slug          = models.SlugField(unique=True)
+    description   = models.CharField(max_length=80, blank=True, null=True)
+    left          = models.PositiveIntegerField(unique=True)
+    right         = models.PositiveIntegerField(unique=True)
+    is_root       = models.BooleanField()
+    thumbnail     = models.ImageField(upload_to="categories/"
+                                      ,help_text="115x115"
+                                      ,blank=True, null=True)
+    def has_child(self):
+        return self.right - self.left > 1
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('category', (), {'slug': self.slug})
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
 class Post(models.Model):
     title         = models.CharField(max_length=50)
     slug          = models.SlugField(unique_for_date="pub_date")
     description   = models.CharField(max_length=80)
-    main_image    = models.ImageField(upload_to="article_images/originals/",
-                                      help_text="570x270")
-    thumbnail     = models.ImageField(upload_to="article_images/thumbnails/",
-                                      help_text="115x115")
+    main_image    = models.ImageField(upload_to="article_images/originals/"
+                                      ,help_text="570x270")
+    thumbnail     = models.ImageField(upload_to="article_images/thumbnails/"
+                                      ,help_text="115x115")
     content       = models.TextField()
     pub_date      = models.DateTimeField(auto_now_add=True)
     edit_date     = models.DateTimeField(auto_now=True)
@@ -27,8 +49,13 @@ class Post(models.Model):
     def __unicode__(self):
         return "%d - %s" % (self.id, self.title)
 
+    @models.permalink
     def get_absolute_url(self):
-        return self.pub_date.strftime("%Y/%m/%d") + "/" + self.slug
+        return ('post', (), {'year': self.pub_date.year
+                             ,'month': self.pub_date.month
+                             ,'day': self.pub_date.day
+                             ,'slug': self.slug
+                             })
 
     def save(self, *arg, **kwargs):
         if self.pk is not None:
