6a5b3fc0eb53c4deb28491d995ca4a7d37267bd8
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-09-03 16:00:57 +0200

    Adds urls

--- nivls_website/blog/urls.py
+++ nivls_website/blog/urls.py
@@ -2,5 +2,22 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'nivls_website.blog.views.home', name='blog'),
+    url(r'^$', 'nivls_website.blog.views.home',
+        name='home'),
+
+    url(r'^((?P<year>\d{4})(/(?P<month>\d{2})(/(?P<day>\d{2}))?)?)/(page-(?P<page>[1-9]+)/)?$',
+        'nivls_website.blog.views.post_list_by_archives',
+        name='archives'),
+
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-w]+)/$',
+        'nivls_website.blog.views.post',
+        name='post'),
+
+    url(r'^categories/(?P<slug>[-w]+)/(page-(?P<page>\d+)/)?$',
+        'nivls_website.blog.views.post_list_by_categories',
+        name='category'),
+
+    url(r'^tags/(?P<slug>[-w]+)/(page-(?P<page>\d+)/)?$',
+        'nivls_website.blog.views.post_list_by_tags',
+        name='tag'),
 )
--- nivls_website/blog/views.py
+++ nivls_website/blog/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, "blog/home.html")
 
+def post(request, year, month, day, slug):
+    return render(request, "blog/home.html")
+
+def post_list_by_categories(request, slug, page):
+    return render(request, "blog/home.html")
+
+def post_list_by_tags(request, slug, page):
+    return render(request, "blog/home.html")
+
+def post_list_by_archives(request, year, month, day, page):
+    return render(request, "blog/home.html")
