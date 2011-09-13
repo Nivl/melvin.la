3a6bb2a0db69ab24f36737c04707d50874d5bd78
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-09-13 22:25:40 +0200

    Adds error 404 page

---
+++ nivls_website/static/js/errors.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#parallax').jparallax({}, {},
+
+			     {ytravel: '100px',
+			      xtravel: '400px'},
+
+			     {ytravel: '50px',
+			      xtravel: '50px'},
+
+			     {ytravel: '50px',
+			      xtravel: '100px'},
+
+			     {ytravel: '200px',
+			      xtravel: '200px'},
+
+			     {ytravel: '50px',
+			      xtravel: '50px'});
+});
