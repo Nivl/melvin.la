2db19a4aeae20870110a3d441860ee0174de84fd
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-09-13 22:49:12 +0200

    Query optimization

--- nivls_website/blog/views.py
+++ nivls_website/blog/views.py
@@ -28,7 +28,7 @@ def contact(request):
 
 
 def home(request):
-    post_list = Post.objects.filter(is_public=1).order_by('-pub_date')
+    post_list = Post.objects.select_related().filter(is_public=1).order_by('-pub_date')
     posts = simple_paginator(post_list, 10, request.GET.get('page'))
     return render(request, "blog/home.html", {"posts": posts})
 
@@ -46,7 +46,8 @@ def post_list_by_categories(request, slug):
     cat = get_object_or_404(Category, slug=slug)
     cat_list = Category.objects.filter(left__gte=cat.left,
                                        left__lte=cat.right)
-    post_list = Post.objects.filter(category__in=cat_list, is_public=1).order_by('-pub_date')
+    post_list = Post.objects.select_related().filter(category__in=cat_list,
+                                                     is_public=1).order_by('-pub_date')
     posts = simple_paginator(post_list, 10, request.GET.get('page'))
     return render(request, "blog/post_list.html", {"posts": posts,
                                                    "obj_name": cat.name})
@@ -54,7 +55,7 @@ def post_list_by_categories(request, slug):
 
 def post_list_by_tags(request, slug):
     tag = get_object_or_404(Tag, slug=slug)
-    post_list = Post.objects.filter(tags=tag, is_public=1).order_by('-pub_date')
+    post_list = Post.objects.select_related().filter(tags=tag, is_public=1).order_by('-pub_date')
     posts = simple_paginator(post_list, 10, request.GET.get('page'))
     return render(request, "blog/post_list.html", {"posts": posts,
                                                    "obj_name": tag.name})
@@ -63,16 +64,16 @@ def post_list_by_tags(request, slug):
 def post_list_by_archives(request, year, month=None, day=None):
     if year and month and day:
         archive_date = datetime.date(int(year), int(month), int(day)).strftime("%B, %d %Y")
-        post_list = Post.objects.order_by("-pub_date").filter(
+        post_list = Post.objects.select_related().order_by("-pub_date").filter(
             pub_date__year=year, pub_date__month=month, pub_date__day=day,
             is_public=1)
     elif year and month:
         archive_date = datetime.date(int(year), int(month), 1).strftime("%B %Y")
-        post_list = Post.objects.order_by("-pub_date").filter(
+        post_list = Post.objects.select_related().order_by("-pub_date").filter(
             pub_date__year=year, pub_date__month=month, is_public=1)
     else:
         archive_date = year
-        post_list = Post.objects.order_by("-pub_date").filter(
+        post_list = Post.objects.select_related().order_by("-pub_date").filter(
             pub_date__year=year, is_public=1)
 
     if not post_list:
