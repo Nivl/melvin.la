b1116237a4b74be0d3f113985fdef2ccd14d4f5d
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-09-15 23:26:01 +0200

    Adds post actions in the admin.

--- nivls_website/blog/admin.py
+++ nivls_website/blog/admin.py
@@ -6,6 +6,39 @@ from django.conf import settings
 # Post
 class AdminPost(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    actions = ['make_public', 'make_private', 'allow_comment', 'lock_comment']
+
+    def make_public(self, request, queryset):
+        nb_row = queryset.update(is_public=1)
+        if nb_row == 1:
+            message = "1 post was "
+        else:
+            message = "%s posts were " % nb_row
+        self.message_user(request, "% successfully marked as public" % message)
+
+    def make_private(self, request, queryset):
+        nb_row = queryset.update(is_public=0)
+        if nb_row == 1:
+            message = "1 post was "
+        else:
+            message = "%s posts were " % nb_row
+        self.message_user(request, "% successfully marked as private" % message)
+
+    def lock_comment(self, request, queryset):
+        nb_row = queryset.update(allow_comment=0)
+        if nb_row == 1:
+            message = "1 post was "
+        else:
+            message = "%s posts were " % nb_row
+        self.message_user(request, "% successfully locked" % message)
+
+    def allow_comment(self, request, queryset):
+        nb_row = queryset.update(allow_comment=1)
+        if nb_row == 1:
+            message = "1 post was "
+        else:
+            message = "%s posts were " % nb_row
+        self.message_user(request, "% successfully locked" % message)
 
     class Media:
         js = (settings.STATIC_URL + 'js/blog/admin_post_preview.js',)
