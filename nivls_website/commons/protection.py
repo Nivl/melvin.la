70ef586c9ef2ab1b4abc13e162d019d624847cb2
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-09-18 21:11:33 +0200

    Fixes akismet

--- nivls_website/commons/protection.py
+++ nivls_website/commons/protection.py
@@ -4,12 +4,17 @@ import akismet
 def akismet_is_valid(request, data):
     akismet.USERAGENT = "David Lynch's Python library/1.0"
 
+    if request.subdomain:
+        domain = "http://" + request.subdomain + settings.DOMAIN_NAME
+    else:
+        domain = "http://" + settings.DOMAIN_NAME
+
     try:
         real_key = akismet.verify_key(settings.AKISMET_API_KEY,
-                                      "http://" + settings.DOMAIN_NAME)
+                                      domain)
         if real_key:
             is_spam = akismet.comment_check(settings.AKISMET_API_KEY,
-                                            "http://" + settings.DOMAIN_NAME,
+                                            domain,
                                             request.META['REMOTE_ADDR'],
                                             request.META['HTTP_USER_AGENT'],
                                             comment_content=data)
