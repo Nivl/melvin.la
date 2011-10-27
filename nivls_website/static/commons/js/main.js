766c678263489b3ee1d4575fae2f80cf65c5d39d
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-10-28 00:00:08 +0200

    New about header and new global konami code

---
+++ nivls_website/static/commons/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var konamiCount = 1;
+    $(window).konami(function() {
+	if (konamiCount == 1) {
+	    $("body").css("overflow", "hidden");
+	    dream_force_stop = false;
+	    dream();
+	    ++konamiCount;
+	}
+	else if (konamiCount == 2) {
+	    dream_force_stop = true;
+	    konamiCount = 1
+	}
+    });
+});
