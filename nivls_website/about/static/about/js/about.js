876ab8d5ca895705969cc83b9a1761e2e48724fe
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2011-11-11 18:31:51 +0100

    Updates css and the resume template

--- nivls_website/about/static/about/js/about.js
+++ nivls_website/about/static/about/js/about.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function(){
     $("#site-title").lettering();
+
+    $('.acc_container').hide();
+    $('.acc_trigger:first').addClass('active').next().show();
+
+    $('.acc_trigger').click(function(){
+	if( $(this).next().is(':hidden') ) {
+	    $('.acc_trigger').removeClass('active').next().slideUp();
+	    $(this).toggleClass('active').next().slideDown();
+	}
+	return false;
+    });
 });
