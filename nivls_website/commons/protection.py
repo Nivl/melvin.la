3b39faa14b4fc4ee107a1d2ecb2691fc8f531396
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-01-15 17:46:05 +0100

    Fixes a bug that break the forms.

--- nivls_website/commons/protection.py
+++ nivls_website/commons/protection.py
@@ -1,13 +1,12 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 import akismet
 
 def akismet_is_valid(request, data):
     akismet.USERAGENT = "David Lynch's Python library/1.0"
+    current_site = Site.objects.get_current()
 
-    if request.subdomain:
-        domain = "http://" + request.subdomain + settings.DOMAIN_NAME
-    else:
-        domain = "http://" + settings.DOMAIN_NAME
+    domain = "http://" + current_site.domain + settings.DOMAIN_NAME
 
     try:
         real_key = akismet.verify_key(settings.AKISMET_API_KEY,
