91a070fb14e0ecafece6b4f9f4a1db7196635e56
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-01-27 19:43:18 +0100

    Linked the dot of the header to the 'i'

--- nivls_website/about/static/about/js/about.js
+++ nivls_website/about/static/about/js/about.js
@@ -1,7 +1,24 @@
 
 
 $(document).ready(function(){
+    /* Header */
     $("#site-title").lettering();
+
+    $("#site-title .char16").mouseover(function(){
+	$(this).text("ı");
+	$("#site-title .char15").animate({
+	    left: '0',
+	    top: '0'
+	});
+    }).mouseleave(function(){
+	$("#site-title .char15").animate({
+	    top: '-33',
+	    left: '13'
+	}, "normal", "linear");
+    });
+
+    /***********************************************************/
+    /* Portfolio */
 
     $('.acc_container').hide();
     $('.acc_trigger:first').addClass('active').next().show();
