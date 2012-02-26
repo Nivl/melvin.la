5a938762cf2108a6e502a62aec1649c888b5a219
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-02-26 02:00:32 +0100

    I18n for about and blog: done.

--- nivls_website/blog/views.py
+++ nivls_website/blog/views.py
@@ -8,6 +8,7 @@ from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _
 from django.contrib.sites.models import Site
+from django.template.defaultfilters import date as _date
 from models import Post, Category, Tag
 from forms import ContactForm
 from commons.simple_paginator import simple_paginator
@@ -93,12 +94,12 @@ def post_list_by_tags(request, slug):
 
 def post_list_by_archives(request, year, month=None, day=None):
     if year and month and day:
-        archive_date = datetime.date(int(year), int(month), int(day)).strftime("%B, %d %Y")
+        archive_date = _date(datetime.date(int(year), int(month), int(day)))
         post_list = Post.objects.select_related().order_by("-pub_date").filter(
             pub_date__year=year, pub_date__month=month, pub_date__day=day,
             is_public=1, site=Site.objects.get_current())
     elif year and month:
-        archive_date = datetime.date(int(year), int(month), 1).strftime("%B %Y")
+        archive_date = _date(datetime.date(int(year), int(month), 1), "F Y")
         post_list = Post.objects.select_related().order_by("-pub_date").filter(
             pub_date__year=year, pub_date__month=month, is_public=1,
             site=Site.objects.get_current())
@@ -110,6 +111,6 @@ def post_list_by_archives(request, year, month=None, day=None):
     if not post_list:
         raise Http404
     posts = simple_paginator(post_list, 10, request.GET.get('page'))
-    obj_name = _("Archives from %(date)") % {'date': archive_date}
+    obj_name = _('Archives from %(date)s') % {'date': archive_date}
     return render(request, "blog/post_list.html", {"posts": posts,
                                                    "obj_name": obj_name})
--- nivls_website/settings_fr.py
+++ nivls_website/settings_fr.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from settings import *
 
+DATE_FORMAT = 'd F Y'
 TIME_ZONE = 'Europe/Paris'
 LANGUAGE_CODE = 'fr'
 
