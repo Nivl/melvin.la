ccc2769694ef074881c0cfa9bb9e631e27d47b90
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-02-27 22:49:48 +0100

    [seo] Use I18nSIte instead of Site

--- nivls_website/seo/models.py
+++ nivls_website/seo/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.sites.models import Site
 from django.conf import settings
+from commons.models import I18nSite
 
 class SEO(models.Model):
-    site        = models.ForeignKey(Site, primary_key=True
+    site        = models.ForeignKey(I18nSite, primary_key=True
                                     ,default=settings.SITE_ID)
     title       = models.CharField(max_length=255)
     description = models.TextField()
