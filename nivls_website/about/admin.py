1d51eb38520b2d100a347c2518a2530082ff38f9
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-02-27 18:20:14 +0100

    [about] Cleanup models, improve admin

--- nivls_website/about/admin.py
+++ nivls_website/about/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 from models import *
 
 class CommonAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+
+    def queryset(self, request):
+        return super(CommonAdmin, self).queryset(request).filter(site=settings.SITE_ID)
 
 admin.site.register(StaticInfos)
 admin.site.register(Project, CommonAdmin)
--- nivls_website/about/models.py
+++ nivls_website/about/models.py
@@ -1,19 +1,21 @@
 import os
 from datetime import datetime
 from django.db import models
+from django.conf import settings
 from commons.fields import ColorField
 from lab.models import Project as LabProject
 from commons.models import I18nSite
 
 class StaticInfos(models.Model):
-    site     = models.OneToOneField(I18nSite, primary_key=True)
+    site     = models.OneToOneField(I18nSite, primary_key=True,
+                                    default=settings.SITE_ID)
     about_me = models.TextField()
     profile  = models.TextField()
     skills   = models.TextField()
     awards   = models.TextField()
 
     def __unicode__(self):
-        return self.site.__unicode__()
+        return self.site.language
 
     def save(self, *arg, **kwargs):
         super(StaticInfos, self).save(*arg, **kwargs)
@@ -28,33 +30,44 @@ class StaticInfos(models.Model):
 
 
 class Work(models.Model):
+    site  = models.ForeignKey(I18nSite, default=settings.SITE_ID)
     name  = models.CharField(max_length=255)
-    slug  = models.SlugField(unique=True)
+    slug  = models.SlugField()
     color = ColorField()
 
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        unique_together = ('site', 'slug')
 
 class Field(models.Model):
+    site  = models.ForeignKey(I18nSite, default=settings.SITE_ID)
     name  = models.CharField(max_length=255)
-    slug  = models.SlugField(unique=True)
+    slug  = models.SlugField()
 
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        unique_together = ('site', 'slug')
 
 class Project(models.Model):
+    site        = models.ForeignKey(I18nSite, default=settings.SITE_ID
+                                    ,related_name="%(app_label)s_%(class)s_related")
     name        = models.CharField(max_length=255)
-    slug        = models.SlugField(unique=True)
-    lab         = models.ForeignKey(LabProject, blank=True, null=True)
+    slug        = models.SlugField()
+    lab         = models.ForeignKey(LabProject, blank=True, null=True
+                                    ,limit_choices_to={'site': settings.SITE_ID})
     prod_date   = models.DateField(default=datetime.now)
     screenshot  = models.ImageField(upload_to='site/portfolio/screenshots/',
                                     help_text='258x158 px')
     url         = models.URLField()
     description = models.TextField()
-    works       = models.ManyToManyField(Work)
-    field       = models.ForeignKey(Field)
+    works       = models.ManyToManyField(Work
+                                         ,limit_choices_to={'site': settings.SITE_ID})
+    field       = models.ForeignKey(Field
+                                    ,limit_choices_to={'site': settings.SITE_ID})
     is_personal = models.BooleanField()
 
     def __unicode__(self):
@@ -71,3 +84,6 @@ class Project(models.Model):
             ping_google()
         except Exception:
             pass
+
+    class Meta:
+        unique_together = ('site', 'slug')
