28b2bb96734be31502e143919ddb228592b17780
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-02-28 01:01:29 +0100

    Clean the file

--- nivls_website/commons/views.py
+++ nivls_website/commons/views.py
@@ -1,27 +1,8 @@
 # -*- coding: utf-8 -*-
+import subprocess
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
-#import xhtml2pdf.pisa as pisa
-#import cStringIO as StringIO
-#import cgi
-
-
-# django imports
-import subprocess
-
-
-#def write_pdf(template_src, context_dict):
-#    template = get_template(template_src)
-#    context = Context(context_dict)
-#    html  = template.render(context)
-#    result = StringIO.StringIO()
-#    pdf = pisa.pisaDocument(StringIO.StringIO(
-#            html.encode("UTF-8")), result)
-#    if not pdf.err:
-#        return HttpResponse(result.getvalue(), mimetype='application/pdf')
-#    return HttpResponse('%s' % cgi.escape(html))
-
 
 def write_pdf(template_src, context_dict, output):
     template = get_template(template_src)
