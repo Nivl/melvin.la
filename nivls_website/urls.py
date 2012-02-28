9069d249e8e2db934d7bfccc18ac6e2f677ec51e
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-02-28 01:20:15 +0100

    Add humans.txt

--- nivls_website/urls.py
+++ nivls_website/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
+from commons.views import TexplainView
 
 from blog.urls import sitemaps as smap_blog
 from about.urls import sitemaps as smap_about
@@ -23,4 +25,5 @@ urlpatterns += patterns(
     url(r'^sitemap\.xml$', 'sitemap', {'sitemaps': sitemaps}),
 
     url(r'^robots.txt$', include('robots.urls')),
+    url(r'^humans.txt$', TexplainView.as_view(template_name='humans.txt')),
     )
