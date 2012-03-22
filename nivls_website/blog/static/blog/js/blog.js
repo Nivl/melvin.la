82b1c91284bae661d2d7ec500017109e950d2335
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-03-23 00:38:16 +0100

    Start using bootstrap

--- nivls_website/blog/static/blog/js/blog.js
+++ nivls_website/blog/static/blog/js/blog.js
@@ -1,26 +1,24 @@
-$(function(){
-    $('#slides').slides({
-	preload: true,
-	preloadImage: STATIC_URL + 'blog/img/slideshow/loading.gif',
-	play: 5000,
-	pause: 2500,
-	slideSpeed: 600,
-	hoverPause: true,
+$('#slides').slides({
+    preload: true,
+    preloadImage: STATIC_URL + 'blog/img/slideshow/loading.gif',
+    play: 5000,
+    pause: 2500,
+    slideSpeed: 600,
+    hoverPause: true,
 
-	animationStart: function(current){
-	    $('.caption').animate({
-		bottom: -35
- 	    }, 100);
-	},
-	animationComplete: function(current){
-	    $('.caption').animate({
-		bottom: 0
-	    }, 200);
-	},
-	slidesLoaded: function() {
-	    $('.caption').animate({
-		bottom: 0
-	    }, 200);
-	}
-    });
+    animationStart: function(current){
+	$('.caption').animate({
+	    bottom: -35
+ 	}, 100);
+    },
+    animationComplete: function(current){
+	$('.caption').animate({
+	    bottom: 0
+	}, 200);
+    },
+    slidesLoaded: function() {
+	$('.caption').animate({
+	    bottom: 0
+	}, 200);
+    }
 });
