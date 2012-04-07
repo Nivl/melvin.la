3b8dbdcac5b90e181a3d84029e4f057682bfe770
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-04-07 18:38:24 +0200

    Add downloadable c.v.

--- nivls_website/about/views.py
+++ nivls_website/about/views.py
@@ -1,9 +1,9 @@
+from django.template import RequestContext
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.contrib.sites.models import Site
 from commons.views import write_pdf
 from models import *
-
 
 def home(request):
     profile = get_object_or_404(Profile ,pk=Site.objects.get_current())
@@ -17,10 +17,11 @@ def cv(request):
     return render(request, "about/cv.html", {'sections': sections})
 
 def cv_pdf(request):
-    categories = CVCategory.objects.filter(site=settings.SITE_ID)
-    return write_pdf("about/cv_pdf.html", {'static_infos': static_infos}, "laplanche_melvin.pdf")
+    sections = CVSection.objects.filter(site=settings.SITE_ID)
+    c = RequestContext(request, {'sections': sections})
+    return write_pdf("about/cv_pdf.html", c
+                     ,"cv_laplanche_melvin.pdf")
 
 def portfolio(request):
     projects = WorkProject.objects.filter(site=settings.SITE_ID).select_related().order_by("-prod_date")
     return render(request, "about/portfolio.html", {'projects': projects})
-
