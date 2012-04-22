4eba18d133a2ed1d94d99bcb6a8bab1dd9ca48c4
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-04-22 19:16:36 +0200

    Remove fancybox img old js includes in base.html and add konamicode

--- nivls_website/nivls_website/static/commons/js/main.js
+++ nivls_website/nivls_website/static/commons/js/main.js
@@ -1,32 +1,13 @@
-$.blockUI.defaults.css = { cursor: 'wait'};
-$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
-
-$(document).ready(function(){
-    var konamiCount = 1;
-    $(window).konami(function() {
-	if (konamiCount == 1) {
-	    $("body").css("overflow", "hidden");
-	    dream_force_stop = false;
-	    dream();
-	    ++konamiCount;
-	}
-	else if (konamiCount == 2) {
-	    dream_force_stop = true;
-	    konamiCount = 1
-	}
-    });
-});
-
-$(function() {
-    $(".lab-project").fancybox({
-	'width': 960,
-	'height': 600,
-	'padding': 0,
-	'margin': 0,
-	'autoScale': false,
-	'type': 'iframe',
-	'scrolling': 'no',
-	'overlayColor': '#000',
-	'overlayOpacity': 0.8
-    });
+var konamiCount = 1;
+$(window).konami(function() {
+    if (konamiCount == 1) {
+	$("body").css("overflow", "hidden");
+	dream_force_stop = false;
+	dream();
+	++konamiCount;
+    }
+    else if (konamiCount == 2) {
+	dream_force_stop = true;
+	konamiCount = 1
+    }
 });
