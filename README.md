184bbb571251009d7cc24206fbe2a67232fd79d6
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-04-22 19:01:57 +0200

    Add a markdown preview in the blog comment section

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Requirements
 * [akismet](http://kemayo.wordpress.com/2005/12/02/akismet-py/)
 * [django-bootstrap](https://github.com/earle/django-bootstrap)
 * [django-bootstrap-form](https://github.com/tzangms/django-bootstrap-form)
-* [django-sentry](https://github.com/dcramer/django-sentry)
+* [html5lib](http://code.google.com/p/html5lib/)
 * [wkhtmltopdf](http://code.google.com/p/wkhtmltopdf/) >= 0.11.0 rc1
--- nivls_website/commons/templatetags/common_tags.py
+++ nivls_website/commons/templatetags/common_tags.py
@@ -1,8 +1,16 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 import re
+import html5lib
+from html5lib import sanitizer
 
 register = template.Library()
+
+@register.filter
+def sanitize(value):
+    p = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer)
+    return mark_safe(p.parseFragment(value).toxml())
 
 @register.filter
 def replace_regexp(string, args):
