62ba351da8fe205b292816ef6bc05fa3195b13bd
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-05-20 22:03:23 +0200

    Add new templatetag to print backend list

--- nivls_website/user_profile/templatetags/user_tags.py
+++ nivls_website/user_profile/templatetags/user_tags.py
@@ -1,9 +1,11 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import title
 
 register = template.Library()
 
-@register.filter
+@register.filter(is_safe=True)
 def social_link(user_social):
     username = user_social.extra_data.get('username')
     extra_id = user_social.extra_data.get('id')
@@ -16,3 +18,20 @@ def social_link(user_social):
 
     return mark_safe('<a href="%s">%s</a>'
                      % (providers_links[user_social.provider], username))
+
+@register.filter(is_safe=True)
+def social_sign_in_links(providers, request):
+    output = ""
+
+    for provider in providers:
+        provider = provider.replace('_', '-')
+
+        output += '<li class="%(provider)s-icon"><a rel="tooltip" title="%(title)s" href="%(link)s%(next)s"></a>' % {
+            'link': reverse("socialauth_begin", args=[provider])
+            ,'next': '?next=' + request.get_full_path()
+            ,'provider': provider
+            ,'title': title(provider)
+            }
+
+    return mark_safe(output)
+
