41197ad0a7e8ffe0ad644fc7d03212395542d471
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-05-24 00:21:32 +0200

    End the sign up form (mail output not tested)

--- nivls_website/user_profile/views.py
+++ nivls_website/user_profile/views.py
@@ -1,4 +1,8 @@
 import uuid
+from django.utils.translation import ugettext as _
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.template.loader import render_to_string
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.views import login
@@ -11,18 +15,14 @@ from forms import *
 
 def sign_up(request):
     if request.user.is_authenticated():
-        try:
-            resolve(request.GET.get('next', ''))
-            return HttpResponseRedirect(request.GET.get('next'))
-        except Resolver404:
-            return HttpResponseRedirect(reverse('home'))
+        return HttpResponseRedirect(reverse('home'))
     else:
         if request.method == 'POST':
             form = UserForm(request.POST)
             if form.is_valid():
                 u = User.objects.create_user(form.cleaned_data['username']
-                                              ,form.cleaned_data['email']
-                                              ,form.cleaned_data['password1'])
+                                             ,form.cleaned_data['email']
+                                             ,form.cleaned_data['password1'])
                 u.first_name = form.cleaned_data['first_name'];
                 u.last_name = form.cleaned_data['last_name'];
                 u.is_staff = False;
@@ -32,12 +32,21 @@ def sign_up(request):
                 profile = UserProfile.objects.get(user=u)
                 profile.activation_code = uuid.uuid4()
                 profile.save()
-                #send_mail(subject,
-                #      msg,
-                #      form.cleaned_data['email'],
-                #      [settings.ADMINS[0][1]])
-                return render(request, 'users/signed_up.html')
 
+                subject = _('Your validation link')
+                text_content = render_to_string('users/mail_sign_up.txt'
+                                                ,{'user': u
+                                                  ,'code': profile.activation_code})
+                html_content = render_to_string('users/mail_sign_up.html'
+                                                ,{'user': u
+                                                  ,'code': profile.activation_code})
+                msg = EmailMultiAlternatives(subject
+                                             ,text_content
+                                             ,settings.EMAIL_NO_REPLY
+                                             ,[u.email])
+                msg.attach_alternative(html_content, 'text/html')
+                msg.send(fail_silently = True)
+                return render(request, 'users/signed_up.html')
         else:
             form = UserForm()
         return render(request, "users/sign_up.html", {'form': form})
