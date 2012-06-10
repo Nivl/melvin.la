78759119065ad75743087269725d422619b8a6c4
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-06-10 02:02:34 +0200

    Add a decorator to limit the access to the unlogged users

--- nivls_website/commons/decorators.py
+++ nivls_website/commons/decorators.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseNotAllowed
+from django.http import HttpResponseNotAllowed, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from functools import wraps
 
 
@@ -7,6 +8,16 @@ def ajax_only():
         def inner_decorator(request, *arg, **kwarg):
             if not request.is_ajax():
                 return HttpResponseNotAllowed("Ajax requests only")
+            return func(request, *arg, **kwarg)
+        return wraps(func)(inner_decorator)
+    return decorator
+
+
+def login_forbidden(view='home'):
+    def decorator(func):
+        def inner_decorator(request, *arg, **kwarg):
+            if request.user.is_authenticated():
+                return HttpResponseRedirect(reverse(view))
             return func(request, *arg, **kwarg)
         return wraps(func)(inner_decorator)
     return decorator
