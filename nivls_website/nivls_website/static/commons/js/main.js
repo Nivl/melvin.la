0abecf8678b895a63c0c9c1a16b95774f467e2b9
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-07-02 01:40:26 +0200

    Add the C.V. page and the active link.

--- nivls_website/nivls_website/static/commons/js/main.js
+++ nivls_website/nivls_website/static/commons/js/main.js
@@ -15,6 +15,25 @@ function styleCode() {
 }
 
 $(function() {
+    var navbar_current = $('#navbar-main-list > li.active').offset();
+    var navbar_img = $('<img />');
+    navbar_img.attr('src', STATIC_URL + 'commons/img/navbar-active.png');
+    navbar_img.css('position', 'relative');
+    navbar_img.css('z-index', '1');
+    navbar_img.offset(navbar_current);
+    navbar_img.css('top', '-40px');
+    $('#navbar').append(navbar_img);
+    $('#navbar-main-list > li.active').removeClass('active');
+
+    $('#navbar-main-list > li').click(function(){
+	var left = $(this).offset()['left'];
+	var that = this;
+    	navbar_img.animate({
+	    left: left,
+     	}, 200);
+
+    });
+
     $('[rel=tooltip]').tooltip();
     $('.animated-thumbnails > li').hoverdir();
 
