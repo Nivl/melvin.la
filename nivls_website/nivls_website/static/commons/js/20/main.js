ccb11025e454de049036bea4143c26adcbb67ff7
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-07-15 18:22:45 +0200

    Add full ajax on the lab

--- nivls_website/nivls_website/static/commons/js/20/main.js
+++ nivls_website/nivls_website/static/commons/js/20/main.js
@@ -68,6 +68,21 @@ $(function() {
      	}, 200);
     });
 
+
+    /***********
+     * Lab menu
+     **********/
+    $('#lab-nav-list').on('click', 'a', function() {
+	var active = $('#lab-nav-list').find('.active');
+	active.removeClass('active').addClass('hide');
+	active.next().removeClass('hide');
+
+	var current = $(this).find('.hide');
+	current.removeClass('hide').addClass('active');
+	current.next().addClass('hide');
+    });
+
+
     /***********
      * Ajax
      **********/
