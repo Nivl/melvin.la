7e085a6e757d2f96b343d54d88ad3bb8c7c08b0f
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-07-18 23:02:49 +0200

    Merge home.html and tag.html

--- nivls_website/lab/views.py
+++ nivls_website/lab/views.py
@@ -9,13 +9,7 @@ from models import *
 def home(request):
     project_list = Project.objects.filter(site=settings.SITE_ID)
     projects = simple_paginator(project_list, 5, request.GET.get('page'))
-    return render(request, "lab/home.html", {'projects': projects})
-
-
-@require_safe
-def project(request, slug):
-    p = get_object_or_404(Project, slug=slug)
-    return render(request, "lab/project.html", {'project': p})
+    return render(request, "lab/listing.html", {'projects': projects})
 
 
 @require_safe
@@ -23,5 +17,10 @@ def tag(request, slug):
     tag = get_object_or_404(Tag, slug=slug)
     project_list = Project.objects.filter(site=settings.SITE_ID, tags=tag)
     projects = simple_paginator(project_list, 5, request.GET.get('page'))
-    return render(request, "lab/tag.html", {'projects': projects,
-                                            'tag': tag})
+    return render(request, "lab/listing.html", {'projects': projects,
+                                                'tag': tag})
+
+@require_safe
+def project(request, slug):
+    p = get_object_or_404(Project, slug=slug)
+    return render(request, "lab/project.html", {'project': p})
