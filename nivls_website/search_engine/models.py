679ae373ca6304a254a3f71111edfec5ef9b712a
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-08-04 01:15:01 +0200

    Add the models Item and BlacklistedWord

--- nivls_website/search_engine/models.py
+++ nivls_website/search_engine/models.py
@@ -1,3 +1,26 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    content = models.charfield(
+        maxlength=255,
+        verbose_name=_('content'))
+
+    hit = models.PositiveIntegerField(
+        verbose_name=_('hit'))
+
+    class Meta:
+        ordering = ['hit']
+        verbose_name = _('item')
+        verbose_name_plural = _('items')
+
+
+class BlacklistedWord(models.Model):
+    word = models.charfield(
+        maxlength=255,
+        verbose_name=_('word'))
+
+    class Meta:
+        verbose_name = _('blacklisted word')
+        verbose_name_plural = _('blacklisted words')
