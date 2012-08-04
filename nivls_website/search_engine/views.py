381c1d8b423d91a54f80245ebc0585f967c11899
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-08-04 19:48:42 +0200

    Add the autocomplete view

--- nivls_website/search_engine/views.py
+++ nivls_website/search_engine/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+import json
+import urllib
+from django.http import HttpResponse
+from django.views.decorators.http import require_safe
+from commons.decorators import ajax_only
+from models import *
+
+
+@ajax_only
+@require_safe
+def autocomplete(request):
+    words = ''
+    try:
+        kwords = urllib.unquote_plus(request.GET.get('search', ''))
+        if kwords:
+            words = list(Item.objects.filter(content__istartswith=kwords) \
+                .values_list('content', flat=True)[:10])
+    except:
+        pass
+
+    output = json.dumps(words) if words else ''
+    return HttpResponse(output, content_type="application/json")
