0e2820ebb2dfdc705f004c8e8c080f0698e4ea8d
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-08-07 01:06:45 +0200

    Improve search_engine.Item admin (add filters and actions)

--- nivls_website/search_engine/admin.py
+++ nivls_website/search_engine/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Item)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_filter = ['is_valid']
+    list_display = ['content', 'is_valid']
+    actions = ['make_valid', 'make_invalid']
+
+    def make_valid(self, request, queryset):
+        nb_row = queryset.update(is_valid=1)
+        if nb_row == 1:
+            message = "1 item was "
+        else:
+            message = "%s items were " % nb_row
+        self.message_user(request, "% successfully marked as valid" % message)
+
+    def make_invalid(self, request, queryset):
+        nb_row = queryset.update(is_valid=0)
+        if nb_row == 1:
+            message = "1 item was "
+        else:
+            message = "%s items were " % nb_row
+        self.message_user(request, "% successfully marked as invalid" % message)
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(BlacklistedWord)
