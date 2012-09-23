ecec542e84421e5e31461efbc4d7f37eeb589eb6
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-09-23 23:24:10 +0200

    Hide empty categories in the lab

--- nivls_website/lab/templatetags/tag_lab.py
+++ nivls_website/lab/templatetags/tag_lab.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django import template
 from django.db.models import Count
 from django.contrib.sites.models import Site
@@ -8,8 +9,9 @@ register = template.Library()
 
 @register.inclusion_tag("lab/templatetags/tags.html")
 def lab_tags(act_menu):
-    tags = Tag.objects.filter() \
-                      .annotate(num_project=Count('project__id')) \
+    tags = Tag.objects.annotate(num_project=Count('project')) \
+                      .filter(num_project__gt=0,
+                              project__site=settings.SITE_ID) \
                       .order_by('-num_project', 'name')
     return {'tags': tags,
             'act_menu': act_menu}
