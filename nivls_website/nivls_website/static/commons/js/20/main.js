a9d90ea832e9773f59e83a5f238ea397a61136fe
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-12-21 18:43:48 +0100

    Add micro-data on the breadcrumb

--- nivls_website/nivls_website/static/commons/js/20/main.js
+++ nivls_website/nivls_website/static/commons/js/20/main.js
@@ -373,20 +373,22 @@ function navigationHTML5() {
                 .append('<span class="divider">/<span/>');
         }
 
-        var new_elem = '<li><a';
+        var new_elem = "<li itemtype='http://data-vocabulary.org/Breadcrumb' itemscope><a";
         new_elem += ' data-depth="' + $(that).data('depth') + '"';
         new_elem += ' data-ajax="' + $(that).data('ajax') + '"';
+        new_elem += ' itemprop="url"';
         if ($(that).attr('title') !== undefined) {
             new_elem += ' title="' + $(that).attr('title') + '"';
         }
 
         new_elem += 'href="' + $(that).attr('href') + '">';
+        new_elem += "<span itemprop='title'>";
         if ($(that).attr('title') !== undefined) {
             new_elem += $(that).attr('title');
         } else {
             new_elem += $(that).html();
         }
-        new_elem += '</a>';
+        new_elem += '</span></a>';
         breadcrumb.append(new_elem);
         return breadcrumb;
     }
