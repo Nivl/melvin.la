763572998fa4c0755afc1cf0653c28fb2cdf3219
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-02-22 00:06:36 +0100

    Add an help box (shortcut: '?') - #9

--- nivls_website/nivls_website/static/commons/js/app/global_shortcuts.js
+++ nivls_website/nivls_website/static/commons/js/app/global_shortcuts.js
@@ -1,5 +1,9 @@
 // e: live-edit.js
 
+$(document).bind('keyup.shift_/', function() {
+    $('#help').modal('toggle');
+});
+
 $(document).bind('keyup./', function() {
     $('.form-search .search-query').focus();
 });
