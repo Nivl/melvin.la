4a785c476db0f6a4c5a9a0e2e7249e689612a966
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-03-02 19:52:03 +0100

    Use the editable-wrapper class to handle live editable contents that cannot be simply displayed using .editable

--- nivls_website/nivls_website/static/commons/js/app/live-edit.js
+++ nivls_website/nivls_website/static/commons/js/app/live-edit.js
@@ -16,11 +16,19 @@ uneditElement = function (e) {
 $(document).bind('keydown.e', function() {
     if (live_edit_enabled) {
         uneditElement();
-        $(".editable:reallyEmpty").stop().animate({'min-height': '0'}, function(){$(this).css('background-color', 'white')});
+
+        $(".editable:reallyEmpty").stop().animate({'min-height': '0', 'min-width': '0'}, function(){$(this).css('background-color', 'white')});
         $('#editing-mode-box').stop().animate({'top': '-40'});
         $('#body > header').stop().animate({'margin-top': '0'});
+        $(".editable-wrapper").stop().each(function() {
+            if ($(this).find('.editable:reallyEmpty').length) {
+                $(this).stop().slideToggle();
+            }
+        });
     } else {
-        $(".editable:reallyEmpty").stop().css('background-color', '#9ccfff').animate({'min-height': '30px'});
+        $(".editable-wrapper:hidden").stop().slideToggle({always: function() {
+            $(".editable:reallyEmpty").stop().css('background-color', '#9ccfff').animate({'min-height': '30px', 'min-width': '50px'});
+        }});
         $('#editing-mode-box').stop().animate({'top': '0'});
         $('#body > header').stop().animate({'margin-top': '40px'});
     }
