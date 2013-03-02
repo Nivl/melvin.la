db3209156890ae751aeacf3f3044d5b4e666d675
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-03-02 01:43:52 +0100

    Rewrite animateHighlight. The old animateHighlight is now named flash, and the new one only change the bg color from the current color to a new one. (temporarily breaks the live edit feature)

--- nivls_website/nivls_website/static/commons/js/app/funcs/00-utils.js
+++ nivls_website/nivls_website/static/commons/js/app/funcs/00-utils.js
@@ -5,18 +5,35 @@ function replaceAll(txt, replace, with_this) {
     return txt.replace(new RegExp(replace, 'g'), with_this);
 }
 
-$.fn.animateHighlight = function(highlightColor, duration) {
-    var highlightBg = highlightColor || '#9ccfff';
-    var animateMs = duration || 1500;
-    var originalBg = this.css('backgroundColor');
-    this.stop().css('background-color', highlightBg)
-    .animate({backgroundColor: originalBg}, animateMs);
+$.fn.animateHighlight = function(to, duration) {
+    if (duration === undefined) {
+        duration = 1000;
+    }
+
+    this.stop().animate({backgroundColor: to}, duration);
+    return this;
+};
+
+$.fn.flash = function(color, duration) {
+    color = color || '#9ccfff';
+    duration = duration || 1500;
+    var current = this.css('backgroundColor');
+
+    this.stop().css('background-color', color);
+    this.animateHighlight(current, duration);
+    return this;
+};
+
+
+$.fn.isReallyEmpty = function() {
+    return $.trim($(this).html()).length == 0;
 };
 
 jQuery.extend(jQuery.expr[':'], {
     reallyEmpty: function (el) {
-        return $.trim($(el).html()).length == 0;
-    }});
+        return $(el).isReallyEmpty();
+    }
+});
 
 
 function resizeTextarea() {
