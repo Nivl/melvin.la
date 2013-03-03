4832d699915f77b6512df25ae607cba65ce079f0
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-03-03 18:49:34 +0100

    Fix .editable display

--- nivls_website/nivls_website/static/commons/js/app/live_edit/events.js
+++ nivls_website/nivls_website/static/commons/js/app/live_edit/events.js
@@ -1,21 +1,27 @@
+
 $(document).bind('keydown.e', function() {
-    if (live_edit_enabled) {
+    if (live_edit_enabled === false) {
+        if ($(".editable-wrapper:hidden").length) {
+            $(".editable-wrapper:hidden").stop().slideToggle(function() {
+                $(".editable:reallyEmpty").stop().css('background-color', '#9ccfff').animate({'min-height': '30px', 'min-width': '50px'});
+            });
+        } else {
+            $(".editable:reallyEmpty").stop().css('background-color', '#9ccfff').animate({'min-height': '30px', 'min-width': '50px'});
+        }
+
+        $('#editing-mode-box').stop().animate({'top': '0'});
+        $('#body > header').stop().animate({'margin-top': '40px'});
+    } else {
         uneditElement();
 
         $(".editable:reallyEmpty").stop().animate({'min-height': '0', 'min-width': '0'}, function(){$(this).css('background-color', 'white')});
         $('#editing-mode-box').stop().animate({'top': '-40'});
         $('#body > header').stop().animate({'margin-top': '0'});
         $(".editable-wrapper").stop().each(function() {
-            if ($(this).find('.editable:reallyEmpty').length) {
+            if ($(this).find('.editable').size() == $(this).find('.editable:reallyEmpty').size()) {
                 $(this).stop().slideToggle();
             }
         });
-    } else {
-        $(".editable-wrapper:hidden").stop().slideToggle({always: function() {
-            $(".editable:reallyEmpty").stop().css('background-color', '#9ccfff').animate({'min-height': '30px', 'min-width': '50px'});
-        }});
-        $('#editing-mode-box').stop().animate({'top': '0'});
-        $('#body > header').stop().animate({'margin-top': '40px'});
     }
     live_edit_enabled = !live_edit_enabled;
 });
