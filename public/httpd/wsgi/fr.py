4b8647e4b92e3ae1086f7a244a548d96a3177948
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-03-05 22:34:38 +0100

    Change the virualhost to django_next

--- public/httpd/wsgi/fr.py
+++ public/httpd/wsgi/fr.py
@@ -1,6 +1,6 @@
 import os, sys
 
-sys.path.append('/home/laplan_m/.virtualenvs/nivls_website/lib/python2.7/site-packages/')
+sys.path.append('/home/laplan_m/.virtualenvs/django_next/lib/python2.7/site-packages/')
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", 'nivls_website'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nivls_website.settings.fr")
