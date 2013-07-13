8c3b1a316550d067423784ec13d5e6e1eb7d403e
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-07-13 15:33:48 +0200

    Add user in Cyan (ugly commit)

--- nivls_website/nivls_website/static/commons/js/app/history/utils/move_navbar.js
+++ nivls_website/nivls_website/static/commons/js/app/history/utils/move_navbar.js
@@ -1,6 +1,6 @@
 if (Modernizr.history) {
     function getWorld($target) {
-        var worlds = ['blog', 'lab', 'about', 'portfolio', 'contact'];
+        var worlds = ['blog', 'lab', 'about', 'portfolio', 'contact', 'user'];
 
         for (var i=0; i<worlds.length; ++i) {
             if ($target.hasClass(worlds[i])) {
