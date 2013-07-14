9f77a404475294206f07f54fa5a529d4e2d30659
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-07-15 01:32:12 +0200

    Add lab menu (buggy)

--- nivls_website/lab/templatetags/tag_lab.py
+++ nivls_website/lab/templatetags/tag_lab.py
@@ -7,10 +7,9 @@ register = template.Library()
 
 
 @register.inclusion_tag("lab/templatetags/tags.haml")
-def lab_tags(act_menu):
+def lab_tags():
     tags = Tag.objects.annotate(num_project=Count('project_tags')) \
                       .filter(num_project__gt=0,
                               project_tags__site=settings.SITE_ID) \
                       .order_by('-num_project', 'name')
-    return {'tags': tags,
-            'act_menu': act_menu}
+    return {'tags': tags}
--- nivls_website/nivls_website/static/commons/js/app/misc/funcs/page-menu.js
+++ nivls_website/nivls_website/static/commons/js/app/misc/funcs/page-menu.js
@@ -1,18 +1,31 @@
 $(document).off('click', '#page-menu-icon > a')
-    .on('click', '#page-menu-icon > a', function() {
+    .on('click', '#page-menu-icon > a', function(e) {
 
         if ($('#page-menu-icon').hasClass('clicked') === false) {
-
             $('#page-menu-icon').css('-webkit-transform', 'rotate(180deg)');
             $('#page-menu-icon').css('-ms-transform', 'rotate(180deg)');
             $('#page-menu-icon').css('transform', 'rotate(180deg)');
+            $('#page-menu-icon').css('top', '8px');
+
             $('#page-menu-wrapper').stop().animate({'left': '0'});
-            $('#subtitle-wrapper').stop().animate({'left': '100%'});
+            $('#subtitle-wrapper').stop().animate({'left': '100%'}, function() {
+                $('#subtitle-wrapper').css('display', 'none');
+                $('#bottom-header').css('overflow', 'visible');
+            });
         } else {
+            $('#bottom-header').css('overflow', 'hidden');
+            $('#subtitle-wrapper').css('display', 'block');
+
             $('#page-menu-icon').css('-webkit-transform', 'rotate(0deg)');
+            $('#page-menu-icon').css('-ms-transform', 'rotate(0deg)');
+            $('#page-menu-icon').css('transform', 'rotate(0deg)');
+            $('#page-menu-icon').css('top', '14px');
+
             $('#page-menu-wrapper').stop().animate({'left': '-100%'});
             $('#subtitle-wrapper').stop().animate({'left': '0'});
         }
 
         $('#page-menu-icon').toggleClass('clicked');
+        e.preventDefault();
+        return false;
     });
