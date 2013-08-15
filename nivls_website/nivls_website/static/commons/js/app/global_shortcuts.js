70e178f2ed57697c6c9a2d897d13c96d87233d21
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-15 16:20:25 +0200

    jslint on root of app/

--- nivls_website/nivls_website/static/commons/js/app/global_shortcuts.js
+++ nivls_website/nivls_website/static/commons/js/app/global_shortcuts.js
@@ -1,9 +1,16 @@
+/*global $*/
+/*jslint browser:true */
+
 // e: live-edit.js
 
-$(document).on('keyup', null, 'shift+/', function() {
+$(document).on('keyup', null, 'shift+/', function () {
+    "use strict";
+
     $('#help').modal('toggle');
 });
 
-$(document).on('keyup', null, '/', function() {
+$(document).on('keyup', null, '/', function () {
+    "use strict";
+
     $('.form-search .search-query').focus();
 });
--- nivls_website/nivls_website/static/commons/js/app/main.js
+++ nivls_website/nivls_website/static/commons/js/app/main.js
@@ -1,21 +1,29 @@
-$(function() {
-    var uaParser = new UAParser();
-    var results = uaParser.getResult();
+/*global $, UAParser */
+/*global reloadJsEffects */
+/*jslint browser:true */
 
-    if (results.engine.name != 'WebKit'
-           && results.engine.name != 'Gecko'
-           && $.cookie('browser-warning') !== '1') {
+$(function () {
+    "use strict";
+
+    var uaParser = new UAParser(),
+        results = uaParser.getResult();
+
+    if (results.engine.name !== 'WebKit'
+            && results.engine.name !== 'Gecko'
+            && $.cookie('browser-warning') !== '1') {
         $('#browser-warning').modal('show');
-        $('#browser-warning').find('button').click(function() {
-         $.cookie('browser-warning', '1', {expires: 365, path: '/'});
-     });
+        $('#browser-warning').find('button').click(function () {
+            $.cookie('browser-warning', '1', {expires: 365, path: '/'});
+        });
     }
 
     reloadJsEffects();
 });
 
 
-$(document).on('click', '.scroll', function(e) {
+$(document).on('click', '.scroll', function (e) {
+    "use strict";
+
     e.preventDefault();
     $('html,body').stop().animate({
         scrollTop: $(this.hash).offset().top
