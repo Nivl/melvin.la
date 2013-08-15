f6fe24e13957b2628266a47af86884a64ebe2fd7
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-15 19:11:56 +0200

    jslint on app/history/utils

--- nivls_website/nivls_website/static/commons/js/app/history/utils/get_new_breadcumb.js
+++ nivls_website/nivls_website/static/commons/js/app/history/utils/get_new_breadcumb.js
@@ -1,48 +1,54 @@
-if (Modernizr.history) {
-    function getNewBreadcrumb(that) {
-        var breadcrumb = $('#breadcrumb').clone();
+/*jslint browser:true */
+/*global $ */
 
-        if ($(that).data('depth') === undefined) {
-            return breadcrumb;
-        }
+function getNewBreadcrumb(that) {
+    'use strict';
 
-        breadcrumb.children('li').each(function() {
-            var prev = $(this).prev();
-            var children = $(this).children('a');
-            if (children.data('depth') !== undefined) {
-                if (children.data('depth') >= $(that).data('depth')) {
-                    $(this).nextAll().remove();
-                    $(this).remove();
-                    if (prev.length) {
-                        prev.children('span').remove();
-                    }
+    var breadcrumb = $('#breadcrumb').clone(),
+        new_elem = '';
 
-                }
-            }
-        });
-
-        if (breadcrumb.children('li').last().length) {
-            breadcrumb.children('li').last()
-                .append('<span class="divider">/<span/>');
-        }
-
-        var new_elem = "<li itemtype='http://data-vocabulary.org/Breadcrumb' itemscope><a";
-        new_elem += ' data-depth="' + $(that).data('depth') + '"';
-        new_elem += ' data-ajax="' + $(that).data('ajax') + '"';
-        new_elem += ' itemprop="url"';
-        if ($(that).attr('title') !== undefined) {
-            new_elem += ' title="' + $(that).attr('title') + '"';
-        }
-
-        new_elem += 'href="' + $(that).attr('href') + '">';
-        new_elem += "<span itemprop='title'>";
-        if ($(that).attr('title') !== undefined) {
-            new_elem += $(that).attr('title');
-        } else {
-            new_elem += $(that).html();
-        }
-        new_elem += '</span></a>';
-        breadcrumb.append(new_elem);
+    if ($(that).data('depth') === undefined) {
         return breadcrumb;
     }
+
+    breadcrumb.children('li').each(function () {
+        var prev = $(this).prev(),
+            children = $(this).children('a');
+
+        if (children.data('depth') !== undefined) {
+            if (children.data('depth') >= $(that).data('depth')) {
+                $(this).nextAll().remove();
+                $(this).remove();
+                if (prev.length) {
+                    prev.children('span').remove();
+                }
+
+            }
+        }
+    });
+
+    if (breadcrumb.children('li').last().length) {
+        breadcrumb.children('li').last()
+            .append('<span class="divider">/<span/>');
+    }
+
+    new_elem = "<li itemtype='http://data-vocabulary.org/Breadcrumb' itemscope><a";
+    new_elem += ' data-depth="' + $(that).data('depth') + '"';
+    new_elem += ' data-ajax="' + $(that).data('ajax') + '"';
+    new_elem += ' itemprop="url"';
+    if ($(that).attr('title') !== undefined) {
+        new_elem += ' title="' + $(that).attr('title') + '"';
+    }
+
+    new_elem += 'href="' + $(that).attr('href') + '">';
+    new_elem += "<span itemprop='title'>";
+    if ($(that).attr('title') !== undefined) {
+        new_elem += $(that).attr('title');
+    } else {
+        new_elem += $(that).html();
+    }
+    new_elem += '</span></a>';
+    breadcrumb.append(new_elem);
+
+    return breadcrumb;
 }
--- nivls_website/nivls_website/static/commons/js/app/history/utils/move_navbar.js
+++ nivls_website/nivls_website/static/commons/js/app/history/utils/move_navbar.js
@@ -1,61 +1,78 @@
-if (Modernizr.history) {
-    function getWorld($target) {
-        var worlds = ['blog', 'lab', 'about', 'portfolio', 'contact', 'user'];
+/*jslint browser:true */
+/*global $, Modernizr */
+/*global live_edit_enabled */
 
-        for (var i=0; i<worlds.length; ++i) {
-            if ($target.hasClass(worlds[i])) {
-                return worlds[i];
-            }
+/*
+    live_edit_enabled   : /live_edit/utils.js
+*/
+
+function getWorld($target) {
+    'use strict';
+
+    var worlds = ['blog', 'lab', 'about', 'portfolio', 'contact', 'user'],
+        i = 0;
+
+    for (i = 0; i < worlds.length; i = i + 1) {
+        if ($target.hasClass(worlds[i])) {
+            return worlds[i];
         }
-        return null;
+    }
+    return null;
+}
+
+function moveNavbar(target) {
+    'use strict';
+
+    var $current_active = $('#navbar-main-list > li.active'),
+        current_world = getWorld($current_active),
+        target_world = null;
+
+    if (target !== null && $(target)[0] === $current_active[0]) {
+        return;
     }
 
-    function moveNavbar(target) {
-        var $current_active = $('#navbar-main-list > li.active');
-        var current_world = getWorld($current_active);
+    if (target !== null) {
+        if ($current_active.hasClass('dropdown')) {
+            $('#navbar-main-list .dropdown li.active')
+                .removeClass('active');
 
-        if (target !== null && $(target)[0] == $current_active[0])
-            return;
-
-        if (target !== null) {
-            if ($current_active.hasClass('dropdown')) {
-                $('#navbar-main-list .dropdown li.active')
-                    .removeClass('active');
-
-                if ($(target).parents('.dropdown').length === 0) {
-                    $current_active.removeClass('active');
-                }
-            } else {
+            if ($(target).parents('.dropdown').length === 0) {
                 $current_active.removeClass('active');
-            }
-
-            $(target).addClass('active');
-            var target_world = null;
-
-            // We check if we're in a dropdown, and get the proper "world"
-            if ($(target).parents('.dropdown').length !== 0) {
-                $(target).parents('.dropdown').addClass('active');
-                target_world = getWorld($(target).parents('.dropdown'));
-            } else {
-                target_world = getWorld($(target));
-            }
-
-            if (current_world) {
-                $('body').removeClass(current_world + '-world');
-            }
-            if (target_world) {
-                $('body').addClass(target_world + '-world');
             }
         } else {
             $current_active.removeClass('active');
-            $('#navbar-main-list .dropdown li.active').removeClass('active');
         }
-    }
 
-    $('#navbar-main-list > li a').click(function(e) {
+        $(target).addClass('active');
+        target_world = null;
+
+        // We check if we're in a dropdown, and get the proper "world"
+        if ($(target).parents('.dropdown').length !== 0) {
+            $(target).parents('.dropdown').addClass('active');
+            target_world = getWorld($(target).parents('.dropdown'));
+        } else {
+            target_world = getWorld($(target));
+        }
+
+        if (current_world) {
+            $('body').removeClass(current_world + '-world');
+        }
+        if (target_world) {
+            $('body').addClass(target_world + '-world');
+        }
+    } else {
+        $current_active.removeClass('active');
+        $('#navbar-main-list .dropdown li.active').removeClass('active');
+    }
+}
+
+if (Modernizr.history) {
+    $('#navbar-main-list > li a').click(function (e) {
+        'use strict';
+
         if (live_edit_enabled === false) {
             if ($(this).data('ajax') !== undefined) {
-                if (e.which != 2 && !e.shiftKey && !e.ctrlKey) {
+                if (e.which !== 2 && !e.shiftKey && !e.ctrlKey) {
                     e.preventDefault();
                     moveNavbar($(this).parent('li'));
                 }
@@ -65,9 +82,11 @@ if (Modernizr.history) {
         }
     });
 
-    $(document).on('click', 'a[data-navbar]', function(e) {
+    $(document).on('click', 'a[data-navbar]', function (e) {
+        'use strict';
+
         if (live_edit_enabled === false) {
-            if (e.which != 2 && !e.shiftKey && !e.ctrlKey) {
+            if (e.which !== 2 && !e.shiftKey && !e.ctrlKey) {
                 moveNavbar($(this).data('navbar'));
                 e.preventDefault();
             }
