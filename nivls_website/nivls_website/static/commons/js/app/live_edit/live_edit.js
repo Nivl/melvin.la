cdb775ad1f5db50c4d5d606690f53c9f17364803
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-15 17:48:51 +0200

    jslint on app/live_edit

--- nivls_website/nivls_website/static/commons/js/app/live_edit/live_edit.js
+++ nivls_website/nivls_website/static/commons/js/app/live_edit/live_edit.js
@@ -1,59 +1,81 @@
+/*jslint browser:true */
+/*global $, ajaxPost */
+/*global resolve_urls */
+/*global current_live_edited_element:true, live_edit_enabled:true */
+/*global closeCurrentLiveEdition, markdownEditor, resizeTextarea */
+
+/*
+    closeCurrentLiveEdition             : utils.js
+    live_edit_enabled                   : utils.js
+    current_live_edited_element         : utils.js
+
+    markdownEditor                      : /misc/funcs/markdown_converter.js
+    resizeTextarea                      : /misc/utils/utils.js
+    ajaxPost                            : /ajax/utils.js
+*/
+
 $(document).off('click', '[data-type=live-editable]')
-    .on('click', '[data-type=live-editable]', function(e, close) {
-         if (live_edit_enabled) {
+    .on('click', '[data-type=live-editable]', function (e, close) {
+        'use strict';
+
+        if (live_edit_enabled) {
             closeCurrentLiveEdition(e);
             e.preventDefault();
 
             $(this).removeAttr('data-type');
             current_live_edited_element = this;
 
-            var $editable_elem = $(this);
-            var pk = $(this).data('pk');
-            var pk_name = $(this).data('pk-name') || 'pk';
-            var target = $(this).data('target');
+            var $editable_elem = $(this),
+                pk = $(this).data('pk'),
+                pk_name = $(this).data('pk-name') || 'pk',
+                target = $(this).data('target'),
 
-            var form_url = 'get-' + target + '-form';
-            var value_url = 'get-' + target;
-            var url_data = {};
+                form_url = 'get-' + target + '-form',
+                value_url = 'get-' + target,
+                url_data = {},
+                url = '';
+
             url_data[pk_name] = pk;
 
-            if ($(this).find('>:first-child').prop('tagName') != 'FORM') {
+            if ($(this).find('>:first-child').prop('tagName') !== 'FORM') {
                 // In case the element was empty, we remove the blue bg color
                 $editable_elem.stop().css('background-color', '');
 
-                var url = resolve_urls(form_url, url_data);
+                url = resolve_urls(form_url, url_data);
 
-                $.get(url, function(data) {
-                    $editable_elem.fancyHide('fast', function() {
+                $.get(url, function (data) {
+                    $editable_elem.fancyHide('fast', function () {
                         $editable_elem.html(data);
                         markdownEditor($editable_elem, true);
 
-                        $editable_elem.fadeIn('fast', function() {
+                        $editable_elem.fadeIn('fast', function () {
                             resizeTextarea();
                         });
                     });
 
                     $editable_elem.off('submit', 'form')
-                        .on('submit', 'form', function() {
+                        .on('submit', 'form', function () {
                             ajaxPost(
-                                url, $(this),
-                                function(data, proceed) {
+                                url,
+                                $(this),
+                                function (data, proceed) {
                                     if (proceed) {
-                                    closeCurrentLiveEdition();
+                                        closeCurrentLiveEdition();
+                                    }
                                 }
-                            });
+                            );
                             return false;
                         });
                 });
             } else if (close) {
                 $(this).attr('data-type', 'live-editable');
                 current_live_edited_element = null;
-                var url = resolve_urls(value_url, url_data);
+                url = resolve_urls(value_url, url_data);
 
-                $.get(url, function(data) {
-                    $editable_elem.fadeOut('fast', function() {
+                $.get(url, function (data) {
+                    $editable_elem.fadeOut('fast', function () {
                         $(this).html(data).prettify();
-                        $(this).fancyShow('fast', function() {
+                        $(this).fancyShow('fast', function () {
                             if ($(this).isReallyEmpty()) {
                                 $(this).css('min-height', '30px');
                                 $(this).animateHighlight('#9ccfff');
@@ -62,5 +84,5 @@ $(document).off('click', '[data-type=live-editable]')
                     });
                 });
             }
-         }
+        }
     });
--- nivls_website/nivls_website/static/commons/js/app/live_edit/utils.js
+++ nivls_website/nivls_website/static/commons/js/app/live_edit/utils.js
@@ -1,24 +1,16 @@
+/*jslint browser:true */
+/*global $ */
+
 var current_live_edited_element = null;
 var live_edit_enabled = false;
 
-// function uneditElement(e) {
-//     if (current_live_editing_element != null) {
-//         var $container = $('#' + current_live_editing_element['id']);
-//         if (e === undefined
-//             || ($container.is(e.target) == false
-//                 && $container.has(e.target).length === 0)) {
-//             $container.toggleClass(current_live_editing_element['class']);
-//             $container.trigger('click');
-//         }
-//     }
-// };
-
-
 function closeCurrentLiveEdition(e) {
+    'use strict';
+
     if (current_live_edited_element !== null) {
         var $container = $(current_live_edited_element);
         if (e === undefined
-            || ($container.is(e.target) === false
+                || ($container.is(e.target) === false
                 && $container.has(e.target).length === 0)) {
             $container.attr('data-type', 'live-editable');
             $container.trigger('click', [true]);
