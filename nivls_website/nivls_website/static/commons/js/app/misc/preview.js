f2463e40f0f1a3f070d716f71fe95e71c5117d52
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-15 16:28:53 +0200

    jslint on root of app/misc

--- nivls_website/nivls_website/static/commons/js/app/misc/preview.js
+++ nivls_website/nivls_website/static/commons/js/app/misc/preview.js
@@ -1,10 +1,18 @@
+/*global $, UAParser */
+/*global markdownToHtml */
+/*jslint browser:true */
+
 $(document).off('input keydown wysiwygClicked', '[data-parse]')
-    .on('input keydown wysiwygClicked', '[data-parse]', function() {
+    .on('input keydown wysiwygClicked', '[data-parse]', function () {
+        'use strict';
+
         var target = $(this).data('parse');
         $(target).html(markdownToHtml($(this).val()));
         $(target).prettify();
     });
 
- function loadPreviews() {
+function loadPreviews() {
+    'use strict';
+
     $('[data-storage][value!=""]').trigger('wysiwygClicked');
- }
+}
