075b9224e8da7f61e120f290cdf13c93e58518f1
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-15 18:30:39 +0200

    jslint on app/ajax

--- nivls_website/nivls_website/static/commons/js/app/ajax/forms.js
+++ nivls_website/nivls_website/static/commons/js/app/ajax/forms.js
@@ -1,31 +1,87 @@
+/*jslint browser:true */
+/*global $, django_js_utils, ajaxPost, ajaxFormUpload */
+/*global addCommentAjax, ajaxSwitchElem, ajaxCb_pushBefore, confirmPasswordAjax */
+/*global STATIC_URL */
+
+
+/*
+    ajaxPost            : utils.js
+    ajaxFormUpload      : utils.js
+    ajaxSwitchElem      : utils.js
+    ajaxCb_pushBefore   : utils.js
+
+    addCommentAjax      : /blog/inc/comment_ajax.haml
+    confirmPasswordAjax : /users/inc/password_confirm_ajax.haml
+*/
+
+
+// Callbacks
+
+$(document).on('hidden', '[id^=modal-edit-]', function () {
+    'use strict';
+
+    var target = $(this).find('[id^=edit-][id$=-form]');
+    target.replaceWith('<div class="loading" id="' + target.prop('id') + '"><img src="' + STATIC_URL + '/commons/img/loading-small.gif" alt="loading..."></div>');
+});
+
+
+// HELPERS
+
+function clearError(that) {
+    'use strict';
+
+    $(that).css('background-color', '#fff');
+}
+
+function hideOnSuccess(that) {
+    'use strict';
+
+    $(that).parents('.modal').modal('hide');
+}
+
+function colorOnError(that) {
+    'use strict';
+
+    $(that).parents('.modal').css('background-color', '#f2dede');
+}
+
+
 /******************************************************************************
  * Contact
  *****************************************************************************/
 
 var resolve_urls = django_js_utils.urls.resolve;
 
-$(document).on('submit', '#contact-form', function(){
+$(document).on('submit', '#contact-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxPost(
-        resolve_urls('contact-form'), '#contact-form', function(data, proceed){
-            if (proceed){
+        resolve_urls('contact-form'),
+        '#contact-form',
+        function (data, proceed) {
+            if (proceed) {
                 $(that).before(data);
                 $.jStorage.deleteKey('contact-message');
 
                 ajaxSwitchElem(
                     resolve_urls('contact-form'),
                     '#contact-form',
-                    '*');
+                    '*'
+                );
             }
         },
-        function(){
+        function () {
             ajaxCb_pushBefore(that);
-        });
+        }
+    );
     return false;
 });
 
-$(document).on('submit', '#comment-form', function(){
+$(document).on('submit', '#comment-form', function () {
+    'use strict';
+
     addCommentAjax(this);
     return false;
 });
@@ -34,19 +90,23 @@ $(document).on('submit', '#comment-form', function(){
  * Users
  *****************************************************************************/
 
-$(document).on('submit', '#confirm-password-form', function(){
+$(document).on('submit', '#confirm-password-form', function () {
+    'use strict';
+
     confirmPasswordAjax(this);
     return false;
 });
 
-$(document).on('submit', '#reset-password-form', function(){
+$(document).on('submit', '#reset-password-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxPost(
         resolve_urls('reset-password-form'),
         '#reset-password-form',
-        function(data, proceed){
-            if (proceed){
+        function (data, proceed) {
+            if (proceed) {
                 $(that).before(data);
 
                 ajaxSwitchElem(
@@ -56,77 +116,95 @@ $(document).on('submit', '#reset-password-form', function(){
                 );
             }
         },
-        function(){
+        function () {
             ajaxCb_pushBefore(that);
-        });
+        }
+    );
     return false;
 });
 
 
-$(document).on('submit', '#edit-account-form', function(){
+$(document).on('submit', '#edit-account-form', function () {
+    'use strict';
+
     var that = this;
 
-    ajaxPost(resolve_urls('edit-account-form'), '#edit-account-form',
-                 function(data, proceed){
-                     if (proceed){
-                         hideOnSuccess(that);
-                     }
-                 },
-                 function(){
-                     colorOnError(that);
-                 });
+    ajaxPost(
+        resolve_urls('edit-account-form'),
+        '#edit-account-form',
+        function (data, proceed) {
+            if (proceed) {
+                data();
+                hideOnSuccess(that);
+            }
+        },
+        function () {
+            colorOnError(that);
+        }
+    );
     return false;
 });
 
 
-$(document).on('shown', '#modal-edit-profile', function(){
+$(document).on('shown', '#modal-edit-profile', function () {
+    'use strict';
+
     var that = this;
 
-    $.get(resolve_urls('edit-account-form'), function(data){
+    $.get(resolve_urls('edit-account-form'), function (data) {
         $('#edit-account-form').replaceWith(data);
         clearError(that);
     });
 });
 
 
-$(document).on('submit', '#edit-settings-form', function(){
+$(document).on('submit', '#edit-settings-form', function () {
+    'use strict';
+
     var that = this;
 
-    ajaxPost(resolve_urls('edit-settings-form'), '#edit-settings-form',
-                 function(data, proceed){
-                     if (proceed){
-                         hideOnSuccess(that);
-                     }
-                 },
-                 function(){
-                     colorOnError(that);
-                 });
+    ajaxPost(
+        resolve_urls('edit-settings-form'),
+        '#edit-settings-form',
+        function (data, proceed) {
+            if (proceed) {
+                hideOnSuccess(that);
+            }
+        },
+        function () {
+            colorOnError(that);
+        }
+    );
     return false;
 });
 
 
-$(document).on('shown', '#modal-edit-settings', function(){
+$(document).on('shown', '#modal-edit-settings', function () {
+    'use strict';
+
     var that = this;
 
-    $.get(resolve_urls('edit-settings-form'), function(data){
+    $.get(resolve_urls('edit-settings-form'), function (data) {
         $('#edit-settings-form').replaceWith(data);
         clearError(that);
     });
 });
 
 
-$(document).on('submit', '#edit-password-form', function(){
+$(document).on('submit', '#edit-password-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxPost(
         resolve_urls('edit-password-form'),
         '#edit-password-form',
-        function(data, proceed){
-            if (proceed){
+        function (data, proceed) {
+            if (proceed) {
                 hideOnSuccess(that);
 
                 $('a[href="#' + $('#edit-password-form')
-                  .parents('.modal').prop('id')  + '"]')
+                    .parents('.modal').prop('id')  + '"]')
                     .removeClass('important');
 
                 ajaxSwitchElem(
@@ -140,58 +218,68 @@ $(document).on('submit', '#edit-password-form', function(){
                 );
             }
         },
-        function(){
+        function () {
             colorOnError(that);
-        });
+        }
+    );
     return false;
 });
 
 
-$(document).on('shown', '#modal-edit-password', function(){
+$(document).on('shown', '#modal-edit-password', function () {
+    'use strict';
+
     var that = this;
 
-    $.get(resolve_urls('edit-password-form'), function(data){
+    $.get(resolve_urls('edit-password-form'), function (data) {
         $('#edit-password-form').replaceWith(data);
         clearError(that);
     });
 });
 
-$(document).on('submit', '#edit-email-form', function(){
+$(document).on('submit', '#edit-email-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxPost(
         resolve_urls('edit-email-form'),
         '#edit-email-form',
-        function(data, proceed){
-            if (proceed){
+        function (data, proceed) {
+            if (proceed) {
                 hideOnSuccess(that);
             }
         },
-        function(){
+        function () {
             colorOnError(that);
-        });
+        }
+    );
     return false;
 });
 
 
-$(document).on('shown', '#modal-edit-email', function(){
+$(document).on('shown', '#modal-edit-email', function () {
+    'use strict';
+
     var that = this;
 
-    $.get(resolve_urls('edit-email-form'), function(data){
+    $.get(resolve_urls('edit-email-form'), function (data) {
         $('#edit-email-form').replaceWith(data);
         clearError(that);
     });
 });
 
 
-$(document).on('submit', '#edit-avatar-form', function(){
+$(document).on('submit', '#edit-avatar-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxPost(
         resolve_urls('edit-avatar-form'),
         '#edit-avatar-form',
-        function(data, proceed){
-            if (proceed){
+        function (data, proceed) {
+            if (proceed) {
                 $(that).before(data);
 
                 ajaxSwitchElem(
@@ -201,21 +289,24 @@ $(document).on('submit', '#edit-avatar-form', function(){
                 );
             }
         },
-        function(){
-            ajaxCb_pushBefore(that)
-        });
+        function () {
+            ajaxCb_pushBefore(that);
+        }
+    );
     return false;
 });
 
 
-$(document).on('submit', '#sign-up-form', function(){
+$(document).on('submit', '#sign-up-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxPost(
         resolve_urls('sign-up-form'),
         '#sign-up-form',
-        function(data, proceed){
-            if (proceed){
+        function (data, proceed) {
+            if (proceed) {
                 $(that).before(data);
 
                 ajaxSwitchElem(
@@ -225,21 +316,25 @@ $(document).on('submit', '#sign-up-form', function(){
                 );
             }
         },
-        function(){
-            ajaxCb_pushBefore(that)
-        });
+        function () {
+            ajaxCb_pushBefore(that);
+        }
+    );
     return false;
 });
 
 
-$(document).on('submit', '#profile-picture-form', function(){
+$(document).on('submit', '#profile-picture-form', function () {
+    'use strict';
+
     var that = this;
 
     ajaxFormUpload(
-        resolve_urls('edit-picture-form'), null,
+        resolve_urls('edit-picture-form'),
+        null,
         '#profile-picture-form',
-        function(data, proceed){
-            if (proceed){
+        function (data, proceed) {
+            if (proceed) {
                 $(that).before(data);
 
                 ajaxSwitchElem(
@@ -249,31 +344,9 @@ $(document).on('submit', '#profile-picture-form', function(){
                 );
             }
         },
-        function(){
-            ajaxCb_pushBefore(that)
-        });
+        function () {
+            ajaxCb_pushBefore(that);
+        }
+    );
     return false;
 });
-
-
-// Callbacks
-
-$(document).on('hidden', '[id^=modal-edit-]', function() {
-    var target = $(this).find('[id^=edit-][id$=-form]');
-    target.replaceWith('<div class="loading" id="' + target.prop('id') + '"><img src="' + STATIC_URL + '/commons/img/loading-small.gif" alt="loading..."></div>');
-});
-
-
-// HELPERS
-
-function clearError(that) {
-    $(that).css('background-color', '#fff');
-}
-
-function hideOnSuccess(that) {
-    $(that).parents('.modal').modal('hide');
-}
-
-function colorOnError(that) {
-    $(that).parents('.modal').css('background-color', '#f2dede');
-}
