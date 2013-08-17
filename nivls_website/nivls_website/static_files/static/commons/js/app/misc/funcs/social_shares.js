0856d439ca7acd09ac584809396f49f1d968def2
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-18 00:14:14 +0200

    Add tweeter button

--- nivls_website/nivls_website/static_files/static/commons/js/app/misc/funcs/social_shares.js
+++ nivls_website/nivls_website/static_files/static/commons/js/app/misc/funcs/social_shares.js
@@ -1,10 +1,18 @@
 /*jslint browser:true */
-/*global $, gapi */
+/*global $, gapi, twttr */
 
 function reloadShareButtons() {
     'use strict';
 
     if (gapi !== undefined) {
         gapi.plusone.go();
+    }
+
+    if (twttr.widgets !== undefined) {
+        var $twttr_button = $('.twitter-share-button');
+
+        $twttr_button.attr('data-url', document.location.href);
+        $twttr_button.attr('data-text', $('title').text());
+        twttr.widgets.load();
     }
 }
