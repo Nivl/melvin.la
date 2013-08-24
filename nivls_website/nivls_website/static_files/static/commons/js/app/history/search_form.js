6a535b6c5d1311578d042717525f61a5d0e969b6
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-08-25 00:03:35 +0200

    Fix search form

--- nivls_website/nivls_website/static_files/static/commons/js/app/history/search_form.js
+++ nivls_website/nivls_website/static_files/static/commons/js/app/history/search_form.js
@@ -8,6 +8,25 @@
     changePage  : funcs/change_page.js
 */
 
+function checkAndGet(value) {
+    'use strict';
+
+    var url,
+        query;
+
+    if (value.length > 0) {
+        query = replaceAll(
+            encodeURIComponent(value),
+            '%20',
+            '+'
+        );
+
+        url = resolve_urls('update-typeahead');
+        url += '?search=' + query;
+        $.get(url);
+    }
+}
+
 if (Modernizr.history) {
     $('.search-form input').typeahead({
         name: 'global',
@@ -17,25 +36,31 @@ if (Modernizr.history) {
     $(document).on('typeahead:selected', '.search-form input', function (obj, datum) {
         'use strict';
 
-        var value = datum.value,
-            url = '',
+        var url = '',
             query = '',
             form = $(this).parents('form');
 
-        if (value.length > 0) {
-            query = replaceAll(
-                encodeURIComponent(value),
-                '%20',
-                '+'
-            );
-            url = resolve_urls('update-typeahead');
-            url += '?search=' + query;
-            $.get(url);
-        }
+        checkAndGet(datum.value);
 
         query = form.serialize();
         url = form.attr('action') + '?' + query;
         changePage(url, form.attr('title'), form);
         moveNavbar(null);
+    });
+
+    $(document).on('submit', '.search-form', function (e) {
+        'use strict';
+
+        var query,
+            url;
+
+        checkAndGet($(this).find('input'));
+
+        query = $(this).serialize();
+        url = $(this).attr('action') + '?' + query;
+        changePage(url, $(this).attr('title'), this);
+        moveNavbar(null);
+        e.preventDefault();
+        return false;
     });
 }
