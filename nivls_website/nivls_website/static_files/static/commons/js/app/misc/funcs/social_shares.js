c7d77318f6870e8490ad13ee60ba55033aa08dc1
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-07-06 12:13:39 -0700

    Remove sharing buttons

--- nivls_website/nivls_website/static_files/static/commons/js/app/misc/funcs/social_shares.js
+++ nivls_website/nivls_website/static_files/static/commons/js/app/misc/funcs/social_shares.js
@@ -4,24 +4,24 @@
 function reloadShareButtons() {
     'use strict';
 
-    var $twttr_button = $('.twitter-share-button'),
-        $fb_button = $('.fb-like');
+    // var $twttr_button = $('.twitter-share-button'),
+    //     $fb_button = $('.fb-like');
 
-    if (typeof gapi !== 'undefined') {
-        gapi.plusone.go();
-    }
+    // if (typeof gapi !== 'undefined') {
+    //     gapi.plusone.go();
+    // }
 
-    if (typeof twttr !== 'undefined' && typeof twttr.widgets !== 'undefined') {
-        if ($twttr_button.length > 0) {
-            $twttr_button.attr('data-url', document.location.href);
-            $twttr_button.attr('data-text', $('title').text());
-            twttr.widgets.load();
-        }
-    }
+    // if (typeof twttr !== 'undefined' && typeof twttr.widgets !== 'undefined') {
+    //     if ($twttr_button.length > 0) {
+    //         $twttr_button.attr('data-url', document.location.href);
+    //         $twttr_button.attr('data-text', $('title').text());
+    //         twttr.widgets.load();
+    //     }
+    // }
 
-    if ($fb_button.length > 0) {
-        $fb_button.attr('data-url', document.location.href);
+    // if ($fb_button.length > 0) {
+    //     $fb_button.attr('data-url', document.location.href);
 
-        FB.XFBML.parse(document);
-    }
+    //     FB.XFBML.parse(document);
+    // }
 }
