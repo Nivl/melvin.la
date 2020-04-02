fc946bcc67d61d2deca5ef0ee27b5b900a1b859a
Melvin Laplanche <melvin.wont.reply@gmail.com>
2020-04-01 17:56:48 -0700

    move everything under a 'components' directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import {make as App} from './App/App.bs';
+import {make as App} from './components/App/App.bs';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
