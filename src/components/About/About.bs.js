04c1ef0c7ea7e6d578ceb20a55be00e2d707e426
Melvin Laplanche <melvin.wont.reply@gmail.com>
2020-04-05 18:44:55 -0700

    cleanup css

--- src/components/About/About.bs.js
+++ src/components/About/About.bs.js
@@ -8,7 +8,9 @@ function About(Props) {
   var title = Props.title;
   var content = Props.content;
   var resumeUrl = Props.resumeUrl;
-  return React.createElement(React.Fragment, undefined, React.createElement("h2", undefined, title), React.createElement("hr", undefined), React.createElement("p", undefined, content), React.createElement("a", undefined, resumeUrl));
+  return React.createElement("div", {
+              className: "about"
+            }, React.createElement("h2", undefined, title), React.createElement("p", undefined, content), React.createElement("a", undefined, resumeUrl));
 }
 
 var make = About;
--- src/components/About/About.re
+++ src/components/About/About.re
@@ -2,9 +2,8 @@
 
 [@react.component]
 let make = (~title: string, ~content: string, ~resumeUrl: string) =>
-  <>
+  <div className="about">
     <h2> {React.string(title)} </h2>
-    <hr />
     <p> {React.string(content)} </p>
     <a> {React.string(resumeUrl)} </a>
-  </>;
+  </div>;
