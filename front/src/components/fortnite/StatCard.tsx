0a798948c69577e76bd14a7e14495ae80812b7cb
Melvin Laplanche <melvin.wont.reply@gmail.com>
2024-04-08 00:25:31 -0700

    feat(fortnite): Add level + new StatCard design

--- front/src/components/fortnite/StatCard.tsx
+++ front/src/components/fortnite/StatCard.tsx
@@ -1,12 +1,16 @@
-import { Card, CardBody, CardHeader, Skeleton } from '@nextui-org/react';
-import { Avatar } from '@nextui-org/react';
-import { motion, useMotionTemplate, useMotionValue } from 'framer-motion';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Chip,
+  CircularProgress,
+  Skeleton,
+} from '@nextui-org/react';
 import { MouseEvent } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const StatCard = ({
-  icon,
-  iconSize = '',
+  isProgress = false,
   title,
   stat,
   className = '',
@@ -14,58 +18,52 @@ export const StatCard = ({
   ...delegated
 }: {
   title: string;
-  iconSize?: string;
-  icon: React.ReactNode;
-  stat: string;
+  isProgress?: boolean;
+  stat: string | number;
   className?: string;
   isLoading?: boolean;
 }) => {
-  const mouseX = useMotionValue(0);
-  const mouseY = useMotionValue(0);
-
-  function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
-    const { left, top } = currentTarget.getBoundingClientRect();
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-  }
-
   return (
     <Card
-      onMouseMove={handleMouseMove}
       {...delegated}
       className={twMerge(
-        `group relative w-80 bg-neutral-950 sm:w-72 lg:w-80 ${className}`,
+        `h-[240px] w-[240px] border-none bg-gradient-to-br from-violet-500 to-fuchsia-500 ${className}`,
       )}
     >
-      <motion.div
-        className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
-        style={{
-          background: useMotionTemplate`
-            radial-gradient(
-              100px circle at ${mouseX}px ${mouseY}px,
-              #FF1CF726,
-              transparent 80%
-            )
-          `,
-        }}
-      />
-      <CardHeader className="flex flex-row gap-4">
-        <Avatar
-          icon={icon}
-          classNames={{
-            base: 'bg-violet-950 bg-opacity-80',
-            icon: `text-pink-500 text-[20px] ${iconSize}`,
-          }}
-        />
-        <p className="text-base font-semibold">{title}</p>
-      </CardHeader>
-      <CardBody>
-        <div className="flex flex-col items-center justify-center pb-8">
+      <CardBody className="items-center justify-center pb-0">
+        {isProgress ? (
+          <CircularProgress
+            classNames={{
+              svg: 'w-36 h-36 drop-shadow-md',
+              indicator: 'stroke-white',
+              track: 'stroke-white/10',
+              value: 'text-3xl font-semibold text-white',
+            }}
+            value={isLoading ? undefined : Number(stat) || ~~stat}
+            strokeWidth={4}
+            showValueLabel={true}
+            aria-label="Current level progression"
+          />
+        ) : (
           <div className="text-center text-3xl font-black sm:text-4xl">
-            <Skeleton isLoaded={!isLoading}>{stat}</Skeleton>
+            <Skeleton className="light" isLoaded={!isLoading}>
+              {stat}
+            </Skeleton>
           </div>
-        </div>
+        )}
       </CardBody>
+
+      <CardFooter className="items-center justify-center pt-0">
+        <Chip
+          classNames={{
+            base: 'border-1 border-white/30',
+            content: 'text-white/90 text-small font-semibold',
+          }}
+          variant="bordered"
+        >
+          {title}
+        </Chip>
+      </CardFooter>
     </Card>
   );
 };
