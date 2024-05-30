aa6fbe8958ecfee037d650cf78a806ffdc2798ee
Melvin Laplanche <melvin.wont.reply@gmail.com>
2024-05-29 22:54:47 -0700

    feat: add 404 page with correct theme

---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '404 - Page Not Found :: Melvin Laplanche',
+  description: "This page doesn't exist, sorry about that.",
+};
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="flex">
+        <span className="mr-6 border-r pr-6">404</span>
+        <span>Page not found</span>
+      </h1>
+    </div>
+  );
+}
