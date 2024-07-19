4221d5dcc76c4902ffdb33fe1a5b23ae9a40e4ca
Melvin Laplanche <melvin.wont.reply@gmail.com>
2024-07-18 21:38:53 -0700

    Update google map to use mapIds

--- front/src/components/Home/Map.tsx
+++ front/src/components/Home/Map.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import {
+  AdvancedMarker as Marker,
+  APIProvider,
+  Map as GoogleMap,
+} from '@vis.gl/react-google-maps';
 import { useTheme } from 'next-themes';
 import { memo } from 'react';
 
@@ -9,194 +13,33 @@ const MapContainer = ({
   initialCenter,
 }: {
   className: string;
-  initialCenter: google.maps.LatLng | google.maps.LatLngLiteral;
+  initialCenter: google.maps.LatLngLiteral;
 }) => {
   const { resolvedTheme } = useTheme();
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GCP_MAP_API_KEY || '',
-  });
 
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerClassName={className}
-      center={initialCenter}
-      zoom={12}
-      options={{
-        styles: resolvedTheme === 'dark' ? mapStylesDark : mapStylesLight,
-        scrollwheel: false,
-        keyboardShortcuts: false,
-        disableDoubleClickZoom: true,
-        zoomControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        panControl: false,
-        rotateControl: false,
-        fullscreenControl: false,
-      }}
-    >
-      <Marker position={initialCenter} />
-    </GoogleMap>
-  ) : (
-    <></>
+  const mapId =
+    resolvedTheme === 'dark' ? 'cdfadb8c71aaba12' : '70614a0367adfbc3';
+  return (
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GCP_MAP_API_KEY || ''}>
+      <GoogleMap
+        className={className}
+        defaultCenter={initialCenter}
+        defaultZoom={12}
+        scrollwheel={false}
+        keyboardShortcuts={false}
+        disableDoubleClickZoom={true}
+        zoomControl={false}
+        mapTypeControl={false}
+        scaleControl={false}
+        streetViewControl={false}
+        rotateControl={false}
+        fullscreenControl={false}
+        mapId={mapId}
+      >
+        <Marker position={initialCenter} />
+      </GoogleMap>
+    </APIProvider>
   );
 };
 
 export const Map = memo(MapContainer);
-
-const mapStylesLight = [
-  {
-    featureType: 'landscape.natural',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        visibility: 'on',
-      },
-      {
-        color: '#eaeaea',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        visibility: 'on',
-      },
-      {
-        color: '#cccccc',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      {
-        lightness: 100,
-      },
-      {
-        visibility: 'simplified',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'geometry',
-    stylers: [
-      {
-        visibility: 'on',
-      },
-      {
-        lightness: 700,
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'all',
-    stylers: [
-      {
-        color: '#63B7FF',
-      },
-    ],
-  },
-];
-
-const mapStylesDark = [
-  {
-    featureType: 'landscape.natural',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        visibility: 'on',
-      },
-      {
-        color: '#eaeaea',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        visibility: 'on',
-      },
-      {
-        color: '#cccccc',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      {
-        lightness: 100,
-      },
-      {
-        visibility: 'simplified',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'geometry',
-    stylers: [
-      {
-        visibility: 'on',
-      },
-      {
-        lightness: 700,
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'all',
-    stylers: [
-      {
-        color: '#000000',
-      },
-    ],
-  },
-];
