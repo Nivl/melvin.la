6e3c7361696fd3ca3a9340bc11c90aca6833939f
Melvin Laplanche <melvin.wont.reply@gmail.com>
2024-07-24 23:13:45 -0700

    ci(chromatic): Add multiple viewports and dark/light

---
+++ front/.storybook/modes.ts
@@ -0,0 +1,57 @@
+export const allModes = {
+    xsmall: {
+      viewport: "xsmall",
+    },
+    small: {
+      viewport: "small",
+    },
+    medium: {
+      viewport: "medium",
+    },
+    large: {
+      viewport: "large",
+    },
+    xlarge: {
+      viewport: "xlarge",
+    },
+    xxlarge: {
+      viewport: "xxlarge",
+    },
+    FourK: {
+      viewport: "FourK",
+    },
+    dark: {
+      theme: "dark",
+    },
+    light: {
+      theme: "light",
+    },
+    "light xsmall": {
+      theme: "light",
+      viewport: "xsmall",
+    },
+    "light small": {
+      theme: "light",
+      viewport: "small",
+    },
+    "light medium": {
+      theme: "light",
+      viewport: "medium",
+    },
+    "light large": {
+      theme: "light",
+      viewport: "large",
+    },
+    "light xlarge": {
+      theme: "light",
+      viewport: "xlarge",
+    },
+    "light xxlarge": {
+      theme: "light",
+      viewport: "xxlarge",
+    },
+    "light FourK": {
+      theme: "light",
+      viewport: "FourK",
+    },
+  };
