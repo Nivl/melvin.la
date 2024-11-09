8381ee55a05af1858da2c7b3019658b2453149c0
Melvin Laplanche <melvin.wont.reply@gmail.com>
2024-11-08 18:12:16 -0800

    fix(webapp,dev): Reduce race rate in MSW

--- front/src/components/MswProvider.tsx
+++ front/src/components/MswProvider.tsx
@@ -1,16 +1,34 @@
 'use client';
-import { ReactNode, useEffect } from 'react';
+import { Suspense, use } from 'react';
 
-export const MswProvider = ({ children }: { children: ReactNode }) => {
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_API_MOCKING?.toLowerCase() === 'true') {
-      // eslint-disable-next-line @typescript-eslint/no-require-imports
-      const { enableMSW } = require('../backend/mocks') as {
-        enableMSW: () => void;
-      };
-      enableMSW();
-    }
-  }, []);
+// https://github.com/mswjs/msw/issues/1644
+// https://github.com/mswjs/examples/pull/101/files
+const mockingEnabledPromise =
+  process.env.NEXT_PUBLIC_API_MOCKING?.toLowerCase() === 'true'
+    ? import('../backend/mocks').then(async ({ enableMSW }) => {
+        await enableMSW();
+      })
+    : Promise.resolve();
 
+export function MswProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // If MSW is enabled, we need to wait for the worker to start,
+  // so we wrap the children in a Suspense boundary until it's ready.
+  return (
+    <Suspense fallback={null}>
+      <MSWProviderWrapper>{children}</MSWProviderWrapper>
+    </Suspense>
+  );
+}
+
+function MSWProviderWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  use(mockingEnabledPromise);
   return children;
-};
+}
