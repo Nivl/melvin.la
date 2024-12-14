0cc9b11c09097f50d785507173508a7a530f7156
Melvin Laplanche <melvin.wont.reply@gmail.com>
2024-12-13 21:59:24 -0800

    fix for next 15

--- front/src/app/admin/blog/posts/[id]/page.tsx
+++ front/src/app/admin/blog/posts/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { use } from 'react';
+
 import { EditPost } from '#components/Admin/blog/EditPost';
 
-export default function Home({ params }: { params: { id: string } }) {
-  return <EditPost id={params.id} />;
+export default function Home(props: { params: Promise<{ id: string }> }) {
+  const { id } = use(props.params);
+  return <EditPost id={id} />;
 }
