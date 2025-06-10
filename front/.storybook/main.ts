f311492ed3429a703bb0e4782efe62462d14f5c3
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-06-09 23:45:03 -0700

    build(deps): Update Storybook to v9

--- front/.storybook/main.ts
+++ front/.storybook/main.ts
@@ -4,10 +4,9 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
     "@storybook/addon-themes",
+    '@storybook/addon-docs'
   ],
   framework: {
     name: '@storybook/nextjs',
--- front/src/app/auth/sign-in/page.stories.ts
+++ front/src/app/auth/sign-in/page.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 
 import { Me } from '#backend/types';
 import { useSignIn } from '#hooks/auth/useSignIn.mock';
