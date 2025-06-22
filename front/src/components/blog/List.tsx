537292ab32c0ed9a296b4bb48a31ea0458607cbd
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-06-22 02:01:03 -0700

    fix(blog): use the correct header

--- front/src/components/blog/List.tsx
+++ front/src/components/blog/List.tsx
@@ -1,5 +1,4 @@
 import { PostDetails } from '#components/blog/PostDetails';
-import { Heading } from '#components/layout/Heading';
 import { Section } from '#components/layout/Section';
 import { BlogPost } from '#models/blog/post';
 
@@ -7,7 +6,9 @@ export function List({ posts }: { posts: BlogPost[] }) {
   return (
     <>
       <Section>
-        <Heading level={1}>Blog Posts</Heading>
+        <h1 className="mb-5 pb-5 text-center text-3xl font-black uppercase">
+          Blog Posts
+        </h1>
       </Section>
       <Section>
         <div className="my-auto grid max-w-7xl grid-cols-1 gap-5 p-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
