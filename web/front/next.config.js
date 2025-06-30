ee83d796a53c514916aa1ad3cad68fdff9d0e01b
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-06-29 19:01:56 -0700

    build(web,next): remove unsused rules and redirects

--- web/front/next.config.js
+++ web/front/next.config.js
@@ -6,41 +6,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   transpilePackages: ['next-mdx-remote'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/drrzzmmic/image/upload/**',
-      },
-    ],
-  },
-  redirects() {
-    return [
-      {
-        source: '/conway',
-        destination: '/games/conway',
-        permanent: true,
-      },
-      {
-        source: '/timezones',
-        destination: '/tools/timezones',
-        permanent: true,
-      },
-      {
-        source: '/fortnite',
-        destination: '/tools/fortnite',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
