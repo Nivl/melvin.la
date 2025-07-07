da8e4870f62eda3ebfe89c30b732a9a22d429f3a
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-07-06 21:26:52 -0700

    buid(go): move away from go tools

--- api/generate.go
+++ api/generate.go
@@ -3,6 +3,6 @@
 package melvinla
 
 // generate backend API
-//go:generate go tool github.com/daveshanley/vacuum bundle -q -p openapi/specs openapi/specs/openapi.yml internal/gen/openapi.yml
-//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config openapi/conf/oapi-codegen-server.yml internal/gen/openapi.yml
-//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config openapi/conf/oapi-codegen-models.yml internal/gen/openapi.yml
+//go:generate vacuum bundle -q -p openapi/specs openapi/specs/openapi.yml internal/gen/openapi.yml
+//go:generate oapi-codegen -config openapi/conf/oapi-codegen-server.yml internal/gen/openapi.yml
+//go:generate oapi-codegen -config openapi/conf/oapi-codegen-models.yml internal/gen/openapi.yml
