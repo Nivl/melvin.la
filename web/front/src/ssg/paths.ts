77a943e4fd38836d755c08c97f33bd13a771b3fb
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-07-15 17:46:46 -0700

    fix path

--- web/front/src/ssg/paths.ts
+++ web/front/src/ssg/paths.ts
@@ -1,5 +1,5 @@
 export const BUILD_DIR = '.build-internal';
 export const DB_PATH = `${BUILD_DIR}/build.db`;
 
-const CONTENT_DIR = 'src/content';
+const CONTENT_DIR = 'src/bundled_static/content';
 export const BLOG_POSTS_DIR = `${CONTENT_DIR}/blog`;
