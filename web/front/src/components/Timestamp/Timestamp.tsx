0830c99d5a209efbed5ff09dd092f76297f64753
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-07-15 17:44:07 -0700

    timestamp: copy over the color logic from Timezone

--- web/front/src/components/Timestamp/Timestamp.tsx
+++ web/front/src/components/Timestamp/Timestamp.tsx
@@ -6,8 +6,32 @@ import { MdDeleteForever as DeleteIcon } from 'react-icons/md';
 
 import { Section } from '../layout/Section';
 
+const colors = [
+  'bg-pink-300',
+  'bg-green-400',
+  'bg-blue-400',
+  'bg-amber-400',
+  'bg-teal-300',
+  'bg-sky-300',
+  'bg-indigo-300',
+  'bg-violet-300',
+  'bg-rose-300',
+] as const;
+
+type Color = (typeof colors)[number];
+
+type Data = {
+  date: Date;
+  color: Color;
+};
+
+const getColor = (skip?: Color): Color => {
+  const availableColors = colors.filter(color => color !== skip);
+  return availableColors[Math.floor(Math.random() * availableColors.length)];
+};
+
 export const Timestamp = () => {
-  const [timestamps, setTimestamps] = useState<Date[]>([]);
+  const [timestamps, setTimestamps] = useState<Data[]>([]);
   const [hasError, setHasError] = useState(false);
   const [value, setValue] = useState(NaN);
 
@@ -20,60 +44,76 @@ export const Timestamp = () => {
       </Section>
 
       <Section>
-        <div className="flex flex-col justify-center gap-4">
-          <div className="flex flex-row items-center justify-center gap-4">
-            <NumberInput
-              hideStepper
-              isWheelDisabled
-              isClearable
-              label="Timestamp"
-              className="chromatic-ignore max-w-[284px]"
-              errorMessage={'Invalid timestamp'}
-              isInvalid={hasError}
-              description={`Automatically detects milliseconds, microseconds, and nanoseconds`}
-              value={value}
-              // We don't go above 20 digits because what's the point?
-              // Also when you reach 21 digits, you start having the ability to
-              // input incorrect timestamps
-              maxLength={20}
-              onValueChange={value => {
-                // NaN is used when the input is empty
-                if (!isNaN(value)) {
-                  const len = value.toString().length;
-                  if (len <= 10) {
-                    value = value * 1000; // Convert seconds to milliseconds
-                  } else if (len > 13) {
-                    value = value / Math.pow(10, len - 13); // Convert anything else to milliseconds
-                  }
-                  const date = new Date(value);
-                  const invalid = isNaN(date.getTime());
-                  setHasError(invalid);
-                  if (invalid) {
-                    return;
-                  }
-                  setTimestamps([date, ...timestamps]);
-                  setValue(NaN);
+        <div className="flex flex-col items-center justify-center gap-4">
+          <NumberInput
+            hideStepper
+            isWheelDisabled
+            isClearable
+            label="Timestamp"
+            size="lg"
+            className="chromatic-ignore max-w-[400px]"
+            errorMessage={'Invalid timestamp'}
+            isInvalid={hasError}
+            description={`Automatically detects milliseconds, microseconds, and nanoseconds`}
+            value={value}
+            // We don't go above 20 digits because what's the point?
+            // Also when you reach 21 digits, you start having the ability to
+            // input incorrect timestamps
+            maxLength={20}
+            onValueChange={value => {
+              // NaN is used when the input is empty
+              if (!isNaN(value)) {
+                const len = value.toString().length;
+                if (len <= 10) {
+                  value = value * 1000; // Convert seconds to milliseconds
+                } else if (len > 13) {
+                  value = value / Math.pow(10, len - 13); // Convert anything else to milliseconds
                 }
-              }}
-            />
-          </div>
+                const date = new Date(value);
+                const invalid = isNaN(date.getTime());
+                setHasError(invalid);
+                if (invalid) {
+                  return;
+                }
+                setTimestamps([
+                  ...timestamps,
+                  {
+                    date,
+                    color: getColor(timestamps[timestamps.length - 1]?.color),
+                  },
+                ]);
+                setValue(NaN);
+              }
+            }}
+          />
 
           {timestamps.length > 0 && (
-            <div className="mt-20 flex flex-col content-center gap-4 text-center">
+            <div className="mt-20 flex flex-col gap-4">
               {timestamps.map((timestamp, i) => (
-                <div key={i} className="flex justify-center gap-3">
+                <div
+                  key={i}
+                  className={`${timestamp.color} flex justify-center gap-3 rounded-full p-7 text-black sm:p-4`}
+                >
                   {/* used to break out of the flex container to not have a gap around bold text */}
                   <div>
-                    {Math.floor(timestamp.getTime() / 1000)} is{' '}
+                    {Math.floor(timestamp.date.getTime() / 1000)} is{' '}
                     <span className="font-bold">
-                      {timestamp.getUTCFullYear().toString()}/
-                      {(timestamp.getUTCMonth() + 1)
+                      {timestamp.date.getUTCFullYear().toString()}/
+                      {(timestamp.date.getUTCMonth() + 1)
                         .toString()
                         .padStart(2, '0')}
-                      /{timestamp.getUTCDate().toString().padStart(2, '0')}{' '}
-                      {timestamp.getUTCHours().toString().padStart(2, '0')}:
-                      {timestamp.getUTCMinutes().toString().padStart(2, '0')}:
-                      {timestamp.getUTCSeconds().toString().padStart(2, '0')}{' '}
+                      /{timestamp.date.getUTCDate().toString().padStart(2, '0')}{' '}
+                      {timestamp.date.getUTCHours().toString().padStart(2, '0')}
+                      :
+                      {timestamp.date
+                        .getUTCMinutes()
+                        .toString()
+                        .padStart(2, '0')}
+                      :
+                      {timestamp.date
+                        .getUTCSeconds()
+                        .toString()
+                        .padStart(2, '0')}{' '}
                       UTC
                     </span>
                   </div>
