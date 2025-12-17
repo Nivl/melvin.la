6978a0890bd9aeb70df338f3a3e0513fa8e1b1fd
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-12-17 00:28:19 -0800

    fix linter

--- web/apps/front/e2e/helpers.ts
+++ web/apps/front/e2e/helpers.ts
@@ -1,9 +1,5 @@
 import { expect } from '@playwright/test';
 
-export function expectToBeDefined<T>(value: T | undefined): asserts value is T {
-  expect(value).toBeDefined();
-}
-
 export function expectToBeThruthy<T>(
   value: T | undefined | null,
 ): asserts value is T {
