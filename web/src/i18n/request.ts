9ee230fa11a16cf0c1dbcdd1472f26f999ce62b2
Melvin Laplanche <melvin.wont.reply@gmail.com>
2025-12-27 00:27:00 -0800

    add fallback on en

--- web/src/i18n/request.ts
+++ web/src/i18n/request.ts
@@ -2,10 +2,12 @@
 
 import { getRequestConfig } from 'next-intl/server';
 
+import defaultLocalMessages from '../../messages/en.json';
 import { type Locales, routing } from './routing';
 
+type MessagesType = { [key: string]: string | MessagesType };
+
 export default getRequestConfig(async ({ requestLocale }) => {
-  // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
   if (!locale || !routing.locales.includes(locale as Locales)) {
@@ -13,11 +15,38 @@ export default getRequestConfig(async ({ requestLocale }) => {
   }
 
   const messages = (await import(`../../messages/${locale}.json`)) as {
-    default: Record<string, string>;
+    default: MessagesType;
   };
 
   return {
     locale,
     messages: messages.default,
+    getMessageFallback({ namespace, key }) {
+      const path = [namespace, key].filter(part => part != undefined).join('.');
+      console.log(`Missing translation for ${path} in ${locale}`);
+
+      // We'll try to get that path from the default messages
+      // It shouldn't fail unless we fucked up badly, since we have
+      // type checking in place.
+      const paths = path.split('.');
+      let k: string | MessagesType = defaultLocalMessages as MessagesType;
+      for (const p of paths) {
+        if (typeof k === 'object' && p in k) {
+          k = k[p];
+        } else {
+          // weird case where a parent key is a string instead of an object
+          console.log(`Not found in default local: ${path}`);
+          return paths.at(-1) ?? '???';
+        }
+      }
+
+      if (typeof k === 'string') {
+        return k;
+      } else {
+        // weird case where the key has children instead of being a string
+        console.log(`Not found in default local: ${path}`);
+        return paths.at(-1) ?? '???';
+      }
+    },
   };
 });
