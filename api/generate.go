94e321568501cd638a09a83d0a616851983ef12b
Melvin <Nivl@users.noreply.github.com>
2026-01-18 22:50:33 -0800

    build(api): update dev image (#428)

--- api/generate.go
+++ api/generate.go
@@ -3,6 +3,6 @@
 package melvinla
 
 // generate backend API
-//go:generate vacuum bundle -q -p openapi/specs openapi/specs/openapi.yml internal/gen/openapi.yml
+//go:generate vacuum bundle -c -q -p openapi/specs openapi/specs/openapi.yml internal/gen/openapi.yml
 //go:generate oapi-codegen -config openapi/conf/oapi-codegen-server.yml internal/gen/openapi.yml
 //go:generate oapi-codegen -config openapi/conf/oapi-codegen-models.yml internal/gen/openapi.yml
